40b1c916ab9542e3c1c4a172e222b1601d18317e
Rick Bradley <rick@rickbradley.com>
2009-06-01 15:50:23 -0500

    deployments controller will be RESTful

---
+++ app/controllers/deployments_controller.rb
@@ -0,0 +1,2 @@
+class DeploymentsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :apps, :audits, :customers
+  map.resources :apps, :audits, :customers, :deployments
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
