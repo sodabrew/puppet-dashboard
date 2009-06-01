6539ed727703381a6759987e38e17dcd8cfcd437
Rick Bradley <rick@rickbradley.com>
2009-06-01 15:55:35 -0500

    hosts controller will be RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :apps, :audits, :customers, :deployments, :destinations, :edges
+  map.resources :apps, :audits, :customers, :deployments, :destinations, :edges, :hosts
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
