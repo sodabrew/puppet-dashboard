a8344a5a321b4cbcaa052b7e10c1853a057386eb
Rick Bradley <rick@rickbradley.com>
2009-06-01 15:53:53 -0500

    edges controller will be RESTful

---
+++ app/controllers/edges_controller.rb
@@ -0,0 +1,2 @@
+class EdgesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :apps, :audits, :customers, :deployments, :destinations
+  map.resources :apps, :audits, :customers, :deployments, :destinations, :edges
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
