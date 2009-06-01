4f2bb52f4bc6f81bfa2c1b24ee1366c57de5c059
Rick Bradley <rick@rickbradley.com>
2009-06-01 15:56:47 -0500

    services controller will be RESTful

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,2 @@
+class ServicesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :apps, :audits, :customers, :deployments, :destinations, :edges, :hosts
+  map.resources :apps, :audits, :customers, :deployments, :destinations, :edges, :hosts, :services
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
