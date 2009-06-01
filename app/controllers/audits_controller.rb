0e6904b5038b34966e840166cdc1ffbdce5ced90
Rick Bradley <rick@rickbradley.com>
2009-06-01 15:43:41 -0500

    audits controller will be RESTful

---
+++ app/controllers/audits_controller.rb
@@ -0,0 +1,2 @@
+class AuditsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :customers
+  map.resources :audits, :customers
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
