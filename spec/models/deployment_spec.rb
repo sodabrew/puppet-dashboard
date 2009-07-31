45098b6d73319ced94c86ce1a2b923f9235f5531
Rick Bradley <rick@rickbradley.com>
2009-07-30 21:26:59 -0500

    Basic model and exemplar for Deployments.

--- spec/models/deployment_spec.rb
+++ spec/models/deployment_spec.rb
@@ -1,4 +1,18 @@
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[.. spec_helper]))
 
 describe Deployment do
+  describe 'attributes' do
+    before :each do
+      @deployment = Deployment.new
+    end
+    
+    it 'can be active' do
+      @deployment.should respond_to(:is_active)
+    end
+    
+    it 'should allow setting and retrieving the active status' do
+      @deployment.is_active = true
+      @deployment.is_active.should be_true
+    end
+  end
 end
