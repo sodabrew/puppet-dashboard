a810aefbcacb16f5f86183db75fd6d0ca2e671a3
Rick Bradley <rick@rickbradley.com>
2009-07-31 14:59:09 -0500

    Service relationships form a graph

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,4 +5,34 @@ class Service < ActiveRecord::Base
   has_many :apps
   has_many :source_edges, :class_name => 'Edge', :foreign_key => 'source_id'
   has_many :target_edges, :class_name => 'Edge', :foreign_key => 'target_id'
+  has_many :depends_on_edges, :class_name => 'Edge', :foreign_key => 'source_id'
+  has_many :depends_on, :through => :depends_on_edges, :source => :target
+  has_many :dependent_edges, :class_name => 'Edge', :foreign_key => 'target_id'  
+  has_many :dependents, :through => :dependent_edges, :source => :source
+  
+  def all_depends_on
+    candidates, results, seen = depends_on.dup, [], {}
+    while !candidates.empty?
+      current = candidates.shift
+      unless seen[current]
+        results << current 
+        candidates += current.depends_on
+        seen[current] = true
+      end
+    end
+    results
+  end
+  
+  def all_dependents
+    candidates, results, seen = dependents.dup, [], {}
+    while !candidates.empty?
+      current = candidates.shift
+      unless seen[current]
+        results << current 
+        candidates += current.dependents
+        seen[current] = true
+      end
+    end
+    results
+  end
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -84,5 +84,103 @@ describe Service do
       @service.target_edges.generate!
       @service.target_edges.should_not be_empty
     end
+    
+    it 'should have many "depends on" edges' do
+      @service.should respond_to(:depends_on_edges)
+    end
+
+    it 'should allow assigning "depends on" edges' do
+      @service = Service.generate!
+      @service.depends_on_edges.generate!
+      @service.depends_on_edges.should_not be_empty
+    end
+
+    it 'should have many "dependent" edges' do
+      @service.should respond_to(:dependent_edges)
+    end
+
+    it 'should allow assigning "dependent" edges' do
+      @service = Service.generate!
+      @service.dependent_edges.generate!
+      @service.dependent_edges.should_not be_empty
+    end
+
+    it 'should have many services it depends on' do
+      @service.should respond_to(:depends_on)
+    end
+
+    it 'should allow assigning services it depends on' do
+      @service = Service.generate!
+      @other = Service.generate!
+      @service.depends_on << @service
+      @service.depends_on.should include(@service)
+    end
+
+    it 'should have many dependent services' do
+      @service.should respond_to(:dependents)
+    end
+
+    it 'should allow assigning dependent services' do
+      @service = Service.generate!
+      @other = Service.generate!
+      @other.dependents << @service
+      @other.dependents.should include(@service)
+    end
+    
+    it 'should be able to find all services it depends on' do
+      @service.should respond_to(:all_depends_on)
+    end
+    
+    it 'should be able to find all services that depend on it' do
+      @service.should respond_to(:all_dependents)
+    end
+    
+    describe 'with dependencies' do
+      before :each do
+        @service     = Service.generate!(:name => 'service')
+        @parent      = Service.generate!(:name => 'parent')
+        @grandparent = Service.generate!(:name => 'grandparent')
+        @child       = Service.generate!(:name => 'child')
+        @grandchild  = Service.generate!(:name => 'grandchild')
+        @service.dependents << @parent
+        @parent.dependents  << @grandparent
+        @service.depends_on << @child
+        @child.depends_on   << @grandchild
+      end
+      
+      it 'should not include itself when finding all services it depends on' do
+        @service.all_depends_on.should_not include(@service)
+      end
+      
+      it 'should include services that it directly depends on when finding all services that it depends on' do
+        @service.all_depends_on.should include(@child)
+      end
+    
+      it 'should return all reachable services when finding all services that it depends on' do
+        @service.all_depends_on.should include(@grandchild)      
+      end
+      
+      it 'should only return a single instance of a service that appears multiple times when finding all services that it depends on' do
+        @service.depends_on << @grandchild
+        @service.all_depends_on.select {|s| s == @grandchild }.size.should == 1
+      end
+    
+      it 'should not include itself when finding all services it depends on' do
+        @service.all_dependents.should_not include(@service)
+      end
+    
+      it 'should include services that directly depend on it when finding all services that depend on it' do
+        @service.all_dependents.should include(@parent)
+      end
+
+      it 'should return all reachable services when finding all services that depend on it' do
+        @service.all_dependents.should include(@grandparent)
+      end
+
+      it 'should only return a single instance of a service that appears multiple times when finding all services that depend on it' do
+        @service.dependents << @grandparent
+        @service.all_dependents.select {|s| s == @grandparent }.size.should == 1
+      end
+    end    
   end
 end
