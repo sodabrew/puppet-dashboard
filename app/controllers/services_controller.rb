a1cf25abe8bc3d38226e3b7389484471457b7866
Rick Bradley <rick@rickbradley.com>
2009-08-07 17:21:30 -0500

    small experiment with getting various resource responses RESTfully

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,3 +1,11 @@
 class ServicesController < ApplicationController
   resources_controller_for :service
+
+  response_for :index do |format|
+    format.html # index.rhtml
+    format.js
+    format.xml  { render :xml => resources }
+    format.json { render :json => resources }
+    format.yaml  { render :text => resources.to_yaml }
+  end
 end
