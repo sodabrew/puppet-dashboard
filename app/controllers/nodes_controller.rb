49f7c73fcaddaab8378a57bb1c5af09787e988fe
Rick Bradley <rick@rickbradley.com>
2009-08-25 16:22:25 -0500

    Refactoring parameters processing method.

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -15,17 +15,7 @@ class NodesController < ApplicationController
   
   def update
     @node = Node.find(params[:id])
-
-    if params[:node]
-      if params[:node]['parameters'] and params[:node]['parameters']['key'] and params[:node]['parameters']['value']
-        params[:node]['parameters'] = params[:node]['parameters']['key'].zip(params[:node]['parameters']['value']).inject({}) do |h, pair|
-          h[pair.first] = pair.last unless pair.first.blank?
-          h
-        end
-      else
-        params[:node]['parameters'] = {}
-      end
-    end
+    params[:node]['parameters'] = unserialize_parameters_data(params) if params[:node]  
     
     if @node.update_attributes(params[:node])
       flash[:notice] = "Node was successfully updated."
@@ -34,4 +24,14 @@ class NodesController < ApplicationController
       render :action => 'edit'
     end
   end
+  
+  private
+  
+  def unserialize_parameters_data(params)
+    return {} unless params[:node]['parameters'] and params[:node]['parameters']['key'] and params[:node]['parameters']['value']
+    params[:node]['parameters']['key'].zip(params[:node]['parameters']['value']).inject({}) do |h, pair|
+      h[pair.first] = pair.last unless pair.first.blank?
+      h
+    end
+  end    
 end
