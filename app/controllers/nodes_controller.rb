50d7c7411b8be10211e32f4a0f125afea60c9b86
Rick Bradley <rick@rickbradley.com>
2009-08-25 16:16:13 -0500

    Nodes controller now understands maintaining parameters list.
    Closes:  http://www.pivotaltracker.com/story/show/1057499

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -12,4 +12,26 @@ class NodesController < ApplicationController
   def edit
     @node = Node.find(params[:id])
   end
+  
+  def update
+    @node = Node.find(params[:id])
+
+    if params[:node]
+      if params[:node]['parameters'] and params[:node]['parameters']['key'] and params[:node]['parameters']['value']
+        params[:node]['parameters'] = params[:node]['parameters']['key'].zip(params[:node]['parameters']['value']).inject({}) do |h, pair|
+          h[pair.first] = pair.last unless pair.first.blank?
+          h
+        end
+      else
+        params[:node]['parameters'] = {}
+      end
+    end
+    
+    if @node.update_attributes(params[:node])
+      flash[:notice] = "Node was successfully updated."
+      redirect_to resource_url
+    else
+      render :action => 'edit'
+    end
+  end
 end
--- spec/controllers/nodes_controller_spec.rb
+++ spec/controllers/nodes_controller_spec.rb
@@ -71,14 +71,10 @@ end
 describe NodesController, 'when not integrating' do  
   it_should_behave_like 'a RESTful controller with an index action'
   it_should_behave_like 'a RESTful controller with a new action'
+  it_should_behave_like 'a RESTful controller with a show action'
   it_should_behave_like 'a RESTful controller with a create action'
-  it_should_behave_like 'a RESTful controller with an update action'
   it_should_behave_like 'a RESTful controller with a destroy action'
   it_should_behave_like 'a RESTful controller with an edit action'
-  
-  describe '#show' do
-    
-  end
   
   describe '#show, when YAML is requested' do
     before :each do
@@ -106,6 +102,98 @@ describe NodesController, 'when not integrating' do
     it 'should return the node configuration as the YAML result' do
       do_get
       response.body.should == @node.configuration.to_yaml
+    end
+  end
+  
+  describe '#update' do
+    before :each do
+      @node = Node.generate!
+      @params = { :id => @node.id.to_s, :node => @node.attributes }
+    end
+    
+    def do_put
+      put :update, @params
+    end
+    
+    it 'should fail when an invalid node id is given' do
+      @params[:id] = (@node.id+100).to_s
+      lambda { do_put }.should raise_error(ActiveRecord::RecordNotFound)
+    end
+    
+    describe 'when a valid node id is given' do
+
+      describe 'and the data provided would make the node invalid' do
+        before :each do
+          @params[:node]['name'] = nil
+        end
+        
+        it 'should make the node available to the view' do
+          do_put
+          assigns[:node].should == @node
+        end
+        
+        it 'should not save the node' do
+          do_put
+          Node.find(@node.id).name.should_not be_nil
+        end
+        
+        it 'should have errors on the node' do
+          do_put
+          assigns[:node].errors[:name].should_not be_blank
+        end
+        
+        it 'should render the edit action' do
+          do_put
+          response.should render_template('edit')
+        end
+        
+        it 'should use the default layout' do
+          do_put
+          response.layout.should == 'layouts/application'       
+        end
+      end
+      
+      describe 'and the data provided make the node valid' do
+        it 'should note the update success in flash' do
+          do_put
+          flash[:notice].should match(/success/i)
+        end
+        
+        it 'should update the node with the data provided' do
+          @params[:node]['name'] = 'new name'
+          do_put
+          Node.find(@node.id).name.should == 'new name' 
+        end
+        
+        it 'should redirect to the show action for the node' do
+          do_put
+          response.should redirect_to(node_path(@node))
+        end
+        
+        it 'should set the parameters to an empty hash when the data provides no empty parameters list' do
+          @params[:node].delete('parameters')
+          do_put
+          Node.find(@node.id).parameters.should == {}
+        end
+        
+        it 'should set the parameters to an empty hash when the data provides an empty parameters list' do
+          @params[:node]['parameters'] = {}
+          do_put
+          Node.find(@node.id).parameters.should == {}
+        end
+        
+        it 'should set the parameters to an empty hash when the data provides no non-blank parameter names' do
+          @params[:node]['parameters'] = { 'key' => ['', '', ''], 'value' => ['1', '2', '3'] }
+          do_put
+          Node.find(@node.id).parameters.should == {}          
+        end
+        
+        it 'should set the parameters to a hash based on the data keys and values' do
+          @params[:node]['parameters'] = { 'key' => ['a', '', 'c'], 'value' => ['b', 'x', 'd'] }
+          do_put
+          Node.find(@node.id).parameters.should == { 'a' => 'b', 'c' => 'd' }                    
+        end
+      end
     end
   end
 end
