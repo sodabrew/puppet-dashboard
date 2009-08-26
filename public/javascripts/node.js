64bc2ade89b5d664b4c6ac9d734e8566f5a7ee85
Rick Bradley <rick@rickbradley.com>
2009-08-26 00:18:11 -0500

    jQuery-izing Node<->Service (dis)connect interface
    Closes:  http://www.pivotaltracker.com/story/show/1062671

--- public/javascripts/node.js
+++ public/javascripts/node.js
@@ -6,4 +6,22 @@ jQuery(function($) {
       });
     }
   );
+  
+  $('a.associate-link').livequery(
+    function() {
+      $(this).click(function() {
+        jQuery('div#node-classes').load(this.href);
+        return false;
+      });
+    }
+  );
+
+  $('a.disassociate-link').livequery(
+    function() {
+      $(this).click(function() {
+        jQuery('div#node-classes').load(this.href);
+        return false;
+      });
+    }
+  );
 });
