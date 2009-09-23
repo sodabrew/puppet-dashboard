b8125c4b18a42fa51d07b848ba940346efe6cf77
Rein Henrichs <reinh@reinh.com>
2009-09-23 14:34:43 -0400

    Fix for 1.8.6 Hash[]

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,7 +2,7 @@ class NodesController < ApplicationController
   resources_controller_for :node
 
   before_filter :handle_node_parameters, :only => [:create, :update]
-  
+
   def show
     @node = Node.find(params[:id])
     respond_to do |format|
@@ -15,8 +15,8 @@ class NodesController < ApplicationController
 
   def handle_node_parameters
     if params[:node] && params[:node][:parameters]
-      parameter_pairs = params[:node][:parameters][:key].zip(params[:node][:parameters][:value])
-      params[:node][:parameters] = Hash[parameter_pairs].reject{|k,v| k.blank?}
+      parameter_pairs = params[:node][:parameters][:key].zip(params[:node][:parameters][:value]).flatten
+      params[:node][:parameters] = Hash[*parameter_pairs].reject{|k,v| k.blank?}
     else
       params[:node][:parameters] = {}
     end
