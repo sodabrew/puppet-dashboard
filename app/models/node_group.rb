550cd6121f6aea23032d6ac83eb320fa2354ff20
Rein Henrichs <reinh@reinh.com>
2009-10-30 10:18:08 -0700

    Adding nodes relationship to node group

--- app/models/node_group.rb
+++ app/models/node_group.rb
@@ -2,5 +2,8 @@ class NodeGroup < ActiveRecord::Base
   has_many :node_group_class_memberships
   has_many :node_classes, :through => :node_group_class_memberships
 
+  has_many :node_group_memberships
+  has_many :nodes, :through => :node_group_memberships
+
   serialize :parameters
 end
--- spec/models/node_group_spec.rb
+++ spec/models/node_group_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe NodeGroup do
   before { @node_group = NodeGroup.spawn }
   it { should have_many(:node_classes).through(:node_group_class_memberships) }
+  it { should have_many(:nodes).through(:node_group_memberships) }
 
 
   it 'should allow setting and retrieving parameter values' do
