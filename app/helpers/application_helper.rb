cc09a0c4a23f16aed08757fee999ea57d1a9845c
Rein Henrichs <reinh@reinh.com>
2009-11-03 12:25:52 -0800

    Adding inspector_table helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,23 @@ module ApplicationHelper
       content_tag(:span, name, :class => "current")
     end
   end
+
+  def inspector_table(collection, key=nil, value=nil, options={})
+    unless collection.is_a?(Hash)
+      key ||= :name; value ||= :description
+
+      collection_hash_values = collection.map{ |c|
+        [
+          key.responds_to?(:call) ? key.call(c) : link_to(c.send(key), c),
+          value.responds_to?(:call) ? value.call(c) : c.send(value),
+        ]
+      }.flatten
+
+      collection = Hash[*collection_hash_values]
+    end
+
+    key ||= :key; value ||= :value
+
+    render :partial => 'shared/inspector', :object => collection, :locals => {:key => key.to_s, :value => value.to_s, :options => options}
+  end
 end
