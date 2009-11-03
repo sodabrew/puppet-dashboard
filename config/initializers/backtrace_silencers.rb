0fcd1461cd6b9029918f5d7460a6f2be604eb272
Rein Henrichs <reinh@reinh.com>
2009-11-03 12:23:51 -0800

    Adding unicorn to backtrace silencers

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,3 +5,5 @@
 
 # You can also remove all the silencers if you're trying do debug a problem that might steem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /unicorn/ }
