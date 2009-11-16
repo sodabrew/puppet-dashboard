6d6bbacf3a822033653db9f4cb6d7a2b4c19fe60
Rein Henrichs <reinh@reinh.com>
2009-11-16 13:59:57 -0800

    Handle activity feed events with removed subjects, etc

--- app/models/timeline_event.rb
+++ app/models/timeline_event.rb
@@ -6,4 +6,12 @@ class TimelineEvent < ActiveRecord::Base
   def self.recent(limit = 10)
     all(:order => 'created_at DESC', :limit => limit)
   end
+
+  def subject_name
+    subject ? subject.name : "A #{subject_type.downcase}"
+  end
+
+  def secondary_name
+    secondary_subject ? secondary_subject.name : "A #{secondary_subject_type.downcase}"
+  end
 end
