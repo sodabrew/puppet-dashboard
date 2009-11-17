e59b93902aa98a37815c8debcc7650b3f0bddd7e
Rein Henrichs <reinh@reinh.com>
2009-11-17 15:34:51 -0800

    Refactor node group graph

--- lib/node_group_graph.rb
+++ lib/node_group_graph.rb
@@ -1,12 +1,7 @@
 module NodeGroupGraph
-  def node_group_graph
-    # return @node_group_grpah if @node_group_graph
-    @node_group_graph = Hash[*node_groups.map{|group| [group, compile_node_group_graph(group)]}.flatten]
-  end
-
-  def compile_node_group_graph(group, seen=[])
+  def node_group_graph(group=self, seen=[])
     raise NodeGroupCycleError, group.inspect if seen.include?(group)
     seen << group
-    Hash[*group.node_groups.map{|group| [group, compile_node_group_graph(group, seen)]}.flatten]
+    Hash[*group.node_groups.map{|group| [group, node_group_graph(group, seen)]}.flatten]
   end
 end
