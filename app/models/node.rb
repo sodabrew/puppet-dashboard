cd52252c614d5fee262f9d1c401aaa6e60b72ea6
Rein Henrichs <reinh@reinh.com>
2009-11-17 11:09:15 -0800

    Remove dev comments and alias

--- app/models/node.rb
+++ app/models/node.rb
@@ -50,12 +50,6 @@ class Node < ActiveRecord::Base
   # merging down (preferring parameters specified in node groups that are
   # nearer). Raises a ParameterConflictError if parameters at the same distance
   # from the node have the same name.
-  # def compiled_parameters
-    # return @compiled_parameters if @compiled_parameters
-    # seen_parameters = {}
-    # compile_parameters(self, 0, seen_parameters)
-    # @compiled_parameters = seen_parameters.sort_by{|key, value| key}.inject({}){|compiled, p| compiled.reverse_merge(p[1])}
-  # end
   def compiled_parameters(graph=node_group_graph, depth=1, seen_parameters={0 => parameters.to_hash})
     seen_parameters[depth] ||= {}
     graph.each do |parent, children_graph|
@@ -68,6 +62,4 @@ class Node < ActiveRecord::Base
 
     return seen_parameters.sort_by{|k,v| k}.inject({}){|results, array| depth, parameters = array; results.reverse_merge(parameters)}
   end
-  alias_method :cp, :compiled_parameters
-
 end
