7072ef13e76ee6cc358696e53aaf09e07061d038
Rein Henrichs <reinh@reinh.com>
2009-12-03 11:46:01 -0800

    Moving puppet require to env

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,3 @@
-require 'puppet'
-require 'pp'
 class ReportsController < InheritedResources::Base
   layout 'application'
   protect_from_forgery :except => :create
--- config/environment.rb
+++ config/environment.rb
@@ -52,3 +52,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+require 'puppet'
