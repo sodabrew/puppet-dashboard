15d33c67a3d15b2870ae3e47836b6a9571a313d4
Rein Henrichs <reinh@reinh.com>
2009-12-04 13:40:53 -0800

    Updates to report handling
    * Reports automatically create non-existent nodes
    * Report controller handles raw POST data
    * Reports ordered newest first
    * Report view now uses proxied methods directly on Report objects

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,14 @@
 class ReportsController < InheritedResources::Base
   layout 'application'
   protect_from_forgery :except => :create
+
+  before_filter :handle_raw_post, :only => :create
+
+  private
+
+  def handle_raw_post
+    report = params[:report]
+    return unless report.is_a?(String)
+    params[:report] = {:report => report}
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -6,6 +6,8 @@ class Report < ActiveRecord::Base
 
   delegate :time, :metrics, :logs, :host, :to => :parsed
 
+  default_scope :order => 'created_at DESC'
+
   def status
     return 'failed' if parsed.metrics["resources"][:failed] > 0
     return 'failed' if parsed.metrics["resources"][:failed_restarts] > 0
@@ -13,13 +15,14 @@ class Report < ActiveRecord::Base
   end
 
   def parsed
+    raise "No report data for #{self.inspect}, unable to parse" unless report
     @parsed ||= YAML.load(report)
   end
 
   private
 
   def assign_to_node
-    node = Node.find_by_name(parsed.host)
+    node = Node.find_or_create_by_name(host)
     write_attribute(:node_id, node.id) if node
   end
 end
