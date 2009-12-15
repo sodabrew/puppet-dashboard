522e62ed8f4d7d25eccf1cb35eb23afbcdfa02be
Rein Henrichs <reinh@reinh.com>
2009-12-15 11:43:08 -0800

    Start tracking schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,126 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20091210200419) do
+
+  create_table "assignments", :force => true do |t|
+    t.integer  "node_id"
+    t.integer  "service_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "node_class_memberships", :force => true do |t|
+    t.integer  "node_id"
+    t.integer  "node_class_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "node_classes", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "node_group_class_memberships", :force => true do |t|
+    t.integer  "node_group_id"
+    t.integer  "node_class_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "node_group_edges", :force => true do |t|
+    t.integer  "to_id"
+    t.integer  "from_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "node_group_memberships", :force => true do |t|
+    t.integer  "node_id"
+    t.integer  "node_group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "node_groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "nodes", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "url"
+    t.datetime "reported_at"
+  end
+
+  create_table "parameters", :force => true do |t|
+    t.string   "key"
+    t.text     "value"
+    t.integer  "parameterable_id"
+    t.string   "parameterable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reports", :force => true do |t|
+    t.integer  "node_id"
+    t.text     "report"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "success"
+  end
+
+  create_table "services", :force => true do |t|
+    t.string   "name"
+    t.string   "type"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "timeline_events", :force => true do |t|
+    t.string   "event_type"
+    t.string   "subject_type"
+    t.string   "actor_type"
+    t.string   "secondary_subject_type"
+    t.integer  "subject_id"
+    t.integer  "actor_id"
+    t.integer  "secondary_subject_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                              :null => false
+    t.string   "email"
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
