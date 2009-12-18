6f0906cbfb07fca559ff793deb952f46ea30dd3f
Rein Henrichs <reinh@reinh.com>
2009-12-17 16:10:02 -0800

    [BUGFIX #2947] uneven lists on dashboard
    * Report and activity lists now both limited at 10

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   layout 'secondary_primary'
   def home
     @timeline_events = TimelineEvent.recent(10)
-    @nodes = Node.by_report_date
+    @nodes = Node.by_report_date.all(:limit => 10)
   end
 end
