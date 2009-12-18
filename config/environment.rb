49d7e0296ca3f8cc6a4e2d999edc01f6c9b54645
Rein Henrichs <reinh@reinh.com>
2009-12-17 16:48:09 -0800

    Adding will paginate to vendor and gem.config

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
              :source => 'http://gems.github.com'
   config.gem 'authlogic'
   config.gem 'stringex'
+  config.gem 'will_paginate'
 
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
