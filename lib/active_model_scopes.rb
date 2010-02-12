a08d992d6e064898aad163c85cb393f82d4d9169
Rein Henrichs <reinh@reinh.com>
2010-02-12 13:33:50 -0800

    Add where named scope

--- lib/active_model_scopes.rb
+++ lib/active_model_scopes.rb
@@ -1,6 +1,7 @@
 module ActiveModelScopes
   def self.included(base)
     base.named_scope :limit, lambda{|limit| {:limit => limit}}
+    base.named_scope :where, lambda{|where| {:conditions => where}}
   end
 end
 
