d20c29478ca4d3dced9858a97cd1d4c46ab98ae8
Rein Henrichs <reinh@reinh.com>
2010-03-03 18:01:08 -0800

    Adding run time to dashboard page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @statuses = Status.by_interval(:limit => 30)
+    @reports = Report.all(:limit => 30)
     @timeline_events = TimelineEvent.recent(10)
     @nodes = Node.by_report_date.all(:limit => 10)
   end
