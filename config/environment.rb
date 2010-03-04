5fc4ee8be6f8fd03250293294ef020db9526f6aa
Rein Henrichs <reinh@reinh.com>
2010-03-03 16:14:38 -0800

    Updates to Nodes UI

--- config/environment.rb
+++ config/environment.rb
@@ -51,3 +51,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+ActiveRecord::Base.include_root_in_json = false
