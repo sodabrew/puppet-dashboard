2c95e5d083793e6890992f9ffa1f498402815a99
Rein Henrichs <reinh@reinh.com>
2010-03-29 15:02:52 -0700

    Correct order for reports on dashboard page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @statuses = Status.by_interval(:limit => 30)
-    @reports = Report.all(:limit => 30)
+    @reports = Report.all(:limit => 30, :order => 'time ASC')
     @failed_nodes = Node.failed
     @timeline_events = TimelineEvent.recent(10)
     @nodes = Node.by_report_date.all(:limit => 10)
