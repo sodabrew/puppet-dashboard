b01b55c0254c28c6a81478a5d6550775178ed6bf
Rein Henrichs <rein@puppetlabs.com>
2010-07-26 15:53:39 -0700

    maint: Add flash to the layout and update css
    * Also update button css
    * Use success, notice, error, failure as flash message types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
   def require_user
     unless current_user
       store_location
-      flash[:notice] = "You must be logged in to access this page"
+      flash[:error] = "You must be logged in to access this page"
       redirect_to new_user_session_url
       return false
     end
@@ -36,7 +36,7 @@ class ApplicationController < ActionController::Base
   def require_no_user
     if current_user
       store_location
-      flash[:notice] = "You must be logged out to access this page"
+      flash[:error] = "You must be logged out to access this page"
       redirect_to account_url
       return false
     end
