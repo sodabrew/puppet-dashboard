4951ad530c57e26ee1990898644d5ab67bddb6a8
Matt Robinson <matt@puppetlabs.com>
2010-08-11 11:27:40 -0700

    [#3298] Add a line to the migration to remove truncated reports

--- db/migrate/20100810011613_change_report_report_field_size_to_be_larger.rb
+++ db/migrate/20100810011613_change_report_report_field_size_to_be_larger.rb
@@ -1,5 +1,9 @@
 class ChangeReportReportFieldSizeToBeLarger < ActiveRecord::Migration
   def self.up
+    #First delete truncated reports that cause errors
+    Report.destroy_all(['length(reports.report) = ?', 65535])
+
+    # Next change the report column to a longer length that should handle pretty much any sane report
     change_column( :reports, :report, :mediumtext, :limit => 16.megabytes )
   end
 
