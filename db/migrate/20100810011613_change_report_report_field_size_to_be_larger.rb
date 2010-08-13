779ec923a042f919d69e3db7e0a65da44ebef1dd
Igal Koshevoy <igal@pragmaticraft.com>
2010-08-13 05:00:23 -0700

    [#3298] Add warnings explaining truncation bug and how to re-import reports.

--- db/migrate/20100810011613_change_report_report_field_size_to_be_larger.rb
+++ db/migrate/20100810011613_change_report_report_field_size_to_be_larger.rb
@@ -1,9 +1,11 @@
 class ChangeReportReportFieldSizeToBeLarger < ActiveRecord::Migration
   def self.up
-    #First delete truncated reports that cause errors
-    Report.destroy_all(['length(reports.report) = ?', 65535])
+    count = Report.delete_all(['length(reports.report) = ?', 65535])
+    if count > 0
+      say "WARNING: Deleted #{count} invalid reports that were truncated by the database due to an earlier bug. Please import your reports again (see README for details) if you wish to have these deleted reports re-added to your database correctly."
+    end
 
-    # Next change the report column to a longer length that should handle pretty much any sane report
+    say "Increasing column size of reports to avoid truncation problems:"
     change_column( :reports, :report, :mediumtext, :limit => 16.megabytes )
   end
 
