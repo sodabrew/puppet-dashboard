sodabrew/puppet-dashboard

commit 8124c51ba768f7701d6d29111f0df4a8e1bab5e3

[#4625] Moved pagination from views into controllers
This fixes the problem of every paged index first retrieving all
records, ignoring them, and then retrieving the page it actually wants.
In the case of reports, this was taking a very long time (on the order
of seconds), and is very undesirable. All controllers will still return
the full dataset when requested in YAML or JSON form.

app/controllers/reports_controller.rb
@@ -15,6 +15,10 @@ class ReportsController < InheritedResources::Base
 
   private
 
+  def collection
+    get_collection_ivar || set_collection_ivar(request.format == :html ? end_of_association_chain.paginate(:page => params[:page]) : end_of_association_chain)
+  end
+
   def handle_raw_post
     report = params[:report]
     params[:report] = {}

app/mixins/searchable_index.rb
@@ -2,6 +2,8 @@ module SearchableIndex
   private
 
   def collection
-    get_collection_ivar || set_collection_ivar(end_of_association_chain.search(params[:q] || params[:tag]))
+    coll = end_of_association_chain.search(params[:q] || params[:tag])
+    coll = coll.paginate(:page => params[:page]) if request.format == :html
+    get_collection_ivar || set_collection_ivar(coll)
   end
 end

app/models/node_class.rb
@@ -1,4 +1,6 @@
 class NodeClass < ActiveRecord::Base
+  def self.per_page; 50 end # Pagination
+
   has_many :node_group_class_memberships, :dependent => :destroy
   has_many :node_class_memberships, :dependent => :destroy
 

app/models/node_group.rb
@@ -1,5 +1,8 @@
 class NodeGroup < ActiveRecord::Base
+  def self.per_page; 50 end # Pagination
+
   include NodeGroupGraph
+
   has_many :node_group_class_memberships, :dependent => :destroy
   has_many :node_classes, :through => :node_group_class_memberships
 

app/models/report.rb
@@ -1,4 +1,5 @@
 class Report < ActiveRecord::Base
+  def self.per_page; 20 end # Pagination
   belongs_to :node
 
   validate :report_contains_metrics

app/views/node_classes/index.html.haml
@@ -1,5 +1,3 @@
-- node_classes = paginate_scope(@node_classes)
-
 #sidebar= render 'shared/node_manager_sidebar'
 #main
   .header
@@ -17,17 +15,17 @@
         %th.name
           Name
     %tbody
-      - if node_classes.present?
-        - for node_class in node_classes
+      - if @node_classes.present?
+        - for node_class in @node_classes
           %tr[node_class]
             -# %td.check
               -# = check_box_tag "check_all"
             %td.name
               = link_to h(node_class.name), node_class
-          - if node_classes.total_pages > 1
+          - if @node_classes.total_pages > 1
             %tfoot
               %tr
-                %td= pagination_for node_classes
+                %td= pagination_for @node_classes
       - else
         %tr
           %td

app/views/node_groups/index.html.haml
@@ -1,5 +1,3 @@
-- node_groups = paginate_scope(@node_groups)
-
 #sidebar= render 'shared/node_manager_sidebar'
 #main
   .header
@@ -17,17 +15,17 @@
         %th.name
           Name
     %tbody
-      - if node_groups.present?
-        - for node_group in node_groups
+      - if @node_groups.present?
+        - for node_group in @node_groups
           %tr[node_group]
             -# %td.check
               -# = check_box_tag "check_all"
             %td.name
               = link_to h(node_group.name), node_group
-        - if node_groups.total_pages > 1
+        - if @node_groups.total_pages > 1
           %tfoot
             %tr
-              %td= pagination_for node_groups
+              %td= pagination_for @node_groups
       - else
         %tr
           %td

app/views/reports/index.html.haml
@@ -1,5 +1,3 @@
-- reports = paginate_scope(@reports, :per_page => 20)
-
 #main.full
   .header
     %h2
@@ -7,7 +5,7 @@
       - if @node
         %span.alt for
         = link_to @node.name, @node
-      %span.count== (#{reports.total_entries})
+      %span.count== (#{@reports.total_entries})
 
   .item
     %table.inspector
@@ -25,8 +23,8 @@
           %th Config retrieval
           %th Runtime
       %tbody
-        - if reports.present?
-          - for report in reports
+        - if @reports.present?
+          - for report in @reports
             %tr.nowrap[report]
               = report_status_td(report)
               %td= link_to report.time.to_s, report
@@ -38,11 +36,11 @@
               %td= report.skipped_resources
               %td #{report.config_retrieval_time} s
               %td #{report.total_time} s
-          - if reports.total_pages > 1
+          - if @reports.total_pages > 1
             %tfoot
               %tr
                 %td{:colspan => @node ? 10 : 9}
-                  = pagination_for(reports)
+                  = pagination_for(@reports)
         - else
           %td.empty{:colspan => @node ? 10 : 9}
-            = describe_no_matches_for :reports
+            = describe_no_matches_for :@reports

commit d451af45b19e912727253952cbcfe2f697ca0ad7

[#4801] Fix exception in node's report listing.
The NodesController#report action was failing because it tried to
render a raw array as if it was a paginated array.

This error was introduced in 8124c51b as part of #4625 "Reports index
should only fetch the data it needs" which had an insufficient
refactoring of the pagination.

The existing tests didn't fail because the value was never rendered.
The controller specs set the value, but didn't render it; while the
view specs set their own paginated value and rendered it.

Solution:
* Added `integrate_views` to NodesController to force rendering of
  templates, which caused them to start failing as expected.
* Moved #paginate_scope from ApplicationHelper to PaginateScopeHelper
  and included it in ApplicationController so that all controllers
  could add pagination to their result sets.
* Added pagination to the NodesController#report action for HTML.
* Refactored controllers and mixins to use #paginate_scope, rather
  than reinventing it themselves.
* Eliminated unnecessary #paginate_scope :per_page option.
* Eliminated unused #items_per_page helper.

Signed-off-by: Igal Koshevoy <igal@pragmaticraft.com>

app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 
 class ApplicationController < ActionController::Base
   include InheritedResources::DSL
+  include PaginateScopeHelper
 
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details

app/controllers/nodes_controller.rb
@@ -44,7 +44,7 @@ class NodesController < InheritedResources::Base
     @node = Node.find_by_name!(params[:id])
     @reports = @node.reports
     respond_to do |format|
-      format.html { render 'reports/index' }
+      format.html { @reports = paginate_scope(@reports); render 'reports/index' }
       format.yaml { render :text => @reports.to_yaml, :content_type => 'application/x-yaml' }
       format.json { render :json => @reports.to_json }
     end

app/controllers/reports_controller.rb
@@ -18,7 +18,7 @@ class ReportsController < InheritedResources::Base
   def collection
     get_collection_ivar || set_collection_ivar(
       request.format == :html ? 
-        end_of_association_chain.paginate(:page => params[:page]) : 
+        paginate_scope(end_of_association_chain) : 
         end_of_association_chain
     )
   end

app/helpers/application_helper.rb
@@ -136,23 +136,6 @@ module ApplicationHelper
     (count > 0 && measures_failure) ? 'failure' : 'success'
   end
 
-  ITEMS_PER_PAGE_DEFAULT = 50
-
-  # Return the number of items to put on a paginated page. Override the default
-  # value by setting the ITEMS_PER_PAGE environmental variable.
-  def items_per_page
-    ENV['ITEMS_PER_PAGE'].try(:to_i) || ITEMS_PER_PAGE_DEFAULT
-  end
-
-  # Return a paginated +scope+.
-  def paginate_scope(scope, opts={})
-    opts.reverse_merge!(
-      :page     => params[:page],
-      :per_page => items_per_page
-    )
-    return scope.paginate(opts)
-  end
-
   # Return +collection+ of Puppet::Util::Log objects sorted by their severity level.
   def puppet_log_sorter(collection)
     collection.sort_by do |instance|

app/helpers/paginate_scope_helper.rb
@@ -1,0 +1,7 @@
+module PaginateScopeHelper
+  # Return a paginated +scope+.
+  def paginate_scope(scope, opts={})
+    opts.reverse_merge!(:page => params[:page])
+    return scope.paginate(opts)
+  end
+end

app/mixins/searchable_index.rb
@@ -3,7 +3,7 @@ module SearchableIndex
 
   def collection
     coll = end_of_association_chain.search(params[:q] || params[:tag])
-    coll = coll.paginate(:page => params[:page]) if request.format == :html
+    coll = paginate_scope(coll) if request.format == :html
 
     get_collection_ivar || set_collection_ivar(coll)
   end

spec/controllers/nodes_controller_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe NodesController do
+  integrate_views
+
   describe "#index" do
     before :each do
       @node = Node.generate!
@@ -165,7 +167,7 @@ describe NodesController do
 
         it 'should render the update action' do
           do_put
-          response.should render_template('update')
+          response.should render_template('edit')
         end
       end
 
@@ -194,6 +196,12 @@ describe NodesController do
       specify { response.should be_success }
     end
 
+    shared_examples_for "a paginated reports collection" do
+      it "should be paginated" do
+        assigns[:reports].should be_a_kind_of(WillPaginate::Collection)
+      end
+    end
+
     shared_examples_for "an un-paginated reports collection" do
       it "should not be paginated" do
         assigns[:reports].should_not be_a_kind_of(WillPaginate::Collection)
@@ -212,7 +220,7 @@ describe NodesController do
       before { get :reports, :node => 123 }
 
       it_should_behave_like "a successful reports rendering"
-      it_should_behave_like "an un-paginated reports collection"
+      it_should_behave_like "a paginated reports collection"
     end
 
     context "for YAML" do

spec/helpers/application_helper_spec.rb
@@ -100,44 +100,6 @@ describe ApplicationHelper do
     end
   end
 
-  describe "#items_per_page" do
-    context "when using defaults" do
-      before :each do
-        @per_page = ENV['ITEMS_PER_PAGE']
-        ENV.delete('ITEMS_PER_PAGE')
-      end
-
-      after do
-        ENV['ITEMS_PER_PAGE'] = @per_page if @per_page
-      end
-
-      it "should return the default number of items to paginate by" do
-        helper.items_per_page.should == ApplicationHelper::ITEMS_PER_PAGE_DEFAULT
-      end
-    end
-
-    context "when overriden by environmental variable" do
-      before :each do
-        @per_page = 51;
-        ENV['ITEMS_PER_PAGE'] = @per_page.to_s
-      end
-
-      after do
-        ENV.delete('ITEMS_PER_PAGE')
-      end
-
-      it "should return the overriden number of items to paginate by" do
-        helper.items_per_page.should == @per_page
-      end
-    end
-  end
-
-  describe "#paginate_scope" do
-    it "should paginate the scope" do
-      helper.paginate_scope([1,2,3]).should be_a_kind_of(WillPaginate::Collection)
-    end
-  end
-
   describe "#describe_search_if_present" do
     it "should describe an active search" do
       params[:q] = 'foo'

spec/helpers/paginate_scope_helper_spec.rb
@@ -1,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe PaginateScopeHelper do
+  describe "#paginate_scope" do
+    it "should paginate the scope" do
+      helper.paginate_scope([1,2,3]).should be_a_kind_of(WillPaginate::Collection)
+    end
+  end
+end