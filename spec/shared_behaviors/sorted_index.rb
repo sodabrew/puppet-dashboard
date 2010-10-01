6605aeabb29fa39b1fccb4752900a6b7203db586
Nick Lewis <nick@puppetlabs.com>
2010-10-01 13:56:57 -0400

    (#4661) Fix broken specs for shared behaviors
    These shared behaviors were leaking before blocks into the specs
    using them. Wrapping them in an enclosing describe fixes this and
    alleviates the need to destroy_all before each spec.

--- spec/shared_behaviors/sorted_index.rb
+++ spec/shared_behaviors/sorted_index.rb
@@ -1,14 +1,12 @@
 describe "sorted index", :shared => true do
-  before do
-    model.destroy_all
-  end
+  describe "when retrieving" do
+    it "should be sorted by default" do
+      c = model.generate! :name => "c"
+      b = model.generate! :name => "b"
+      d = model.generate! :name => "d"
+      a = model.generate! :name => "a"
 
-  it "should be sorted by default" do
-    c = model.generate! :name => "c"
-    b = model.generate! :name => "b"
-    d = model.generate! :name => "d"
-    a = model.generate! :name => "a"
-
-    model.all.should == [a,b,c,d]
+      model.all.should == [a,b,c,d]
+    end
   end
 end
