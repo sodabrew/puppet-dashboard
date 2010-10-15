d831eaf9107969f4f20af4d7ef8863078d4b4f63
Nick Lewis <nick@puppetlabs.com>
2010-10-15 15:55:44 -0700

    (#3531) Remove unused NodeGroupCycleError file

--- lib/node_group_cycle_error.rb
+++
@@ -1,7 +0,0 @@
-class NodeGroupCycleError < StandardError
-  attr_reader :node_group
-  def initialize(node_group)
-    @node_group = node_group
-    super("The node groups that this node belongs to contain a cycle.")
-  end
-end
