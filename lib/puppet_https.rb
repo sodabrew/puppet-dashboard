a196fd218185224ff71cbd78f0f40d59d9cb018d
Paul Berry <paul@puppetlabs.com>
2010-10-28 14:23:23 -0700

    (#5149) Added rake tasks to manage certificate and public/private key pair
    Added three rake tasks:
    - create_key_pair: make public/private key pair to represent Dashboard
    - cert_request: upload certificate request to the puppet master
    - cert_retrieve: downloade signed certificate from the puppet master
    
    All three of these tasks must be run in order for Dashboard to access
    puppet REST APIs.
    
    These tasks are implemented in terms of a new lib class, PuppetHttps,
    which contains methods for accessing the puppet REST APIs.

---
+++ lib/puppet_https.rb
@@ -0,0 +1,44 @@
+require 'uri'
+require 'net/https'
+
+class PuppetHttps
+  def self.certificate_path
+    'certificate' # TODO: make configurable
+  end
+
+  def self.private_key_path
+    'id_rsa' # TODO: make configurable
+  end
+
+  def self.public_key_path
+    'id_rsa.pub' # TODO: make configurable
+  end
+
+  def self.make_ssl_request(url, req)
+    connection = Net::HTTP.new(url.host, url.port)
+    connection.use_ssl = true
+    if File.exists?(certificate_path)
+      connection.cert = OpenSSL::X509::Certificate.new(File.read(certificate_path))
+    end
+    if File.exists?(private_key_path)
+      connection.key = OpenSSL::PKey::RSA.new(File.read(private_key_path))
+    end
+    connection.start { |http| http.request(req) }
+  end
+
+  def self.put(url, content_type, data)
+    url = URI.parse(url)
+    req = Net::HTTP::Put.new(url.path)
+    req.content_type = content_type
+    req.body = data
+    res = make_ssl_request(url, req)
+    res.error! unless res.code_type == Net::HTTPOK
+  end
+
+  def self.get(url, accept)
+    url = URI.parse(url)
+    req = Net::HTTP::Get.new(url.path, "Accept" => accept)
+    res = make_ssl_request(url, req)
+    res.body
+  end
+end
