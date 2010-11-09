6dc4b0ced2cfed7ee1e3f4ee0c2bb52ae91c8967
Nick Lewis <nick@puppetlabs.com>
2010-11-08 17:02:13 -0800

    maint: require activesupport from config/environment.rb
    ActiveSupport provides Fixnum#megabytes, which is used when creating the
    Logger.

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'activesupport'
 
 Rails::Initializer.run do |config|
   config.gem 'rack'
