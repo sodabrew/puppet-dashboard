3f5b59f46b4af8b3d9eea2ea05ab66aab3b2e6ec
Nick Lewis <nick@puppetlabs.com>
2010-11-12 15:34:57 -0800

    (#5278) Settings will now individually fallback to values in settings.yml.example
    If neither settings.yml nor settings.yml.example specifies a particular
    setting, it will be nil.
    
    Paired-With: Paul Berry

--- lib/settings_reader.rb
+++ lib/settings_reader.rb
@@ -12,42 +12,41 @@ require 'activesupport'
 # Examples:
 #   # Read from default "config/settings.yml" and "config/settings.yml.example" files:
 #   SETTINGS = SettingsReader.read
-#
 class SettingsReader
   # Return an OpenStruct object with setting information. The settings are read
   # from an ERB-parsed YAML file.
-  #
-  # Arguments:
-  # * Filename to read settings from. Optional, if not given will try
-  #   "config/setting.yml" and "config/setting.yml.example".
-  #
-  # Options:
-  # * :verbose => Print status to screen on error. Defaults to true.
-  def self.read(*args)
-    opts = args.extract_options!
-    verbose = opts[:verbose] != false
-
+  def self.read
     normal_file = "config/settings.yml"
     sample_file = "config/settings.yml.example"
     rails_root = RAILS_ROOT rescue File.dirname(File.dirname(__FILE__))
 
     message = "** SettingsReader - "
 
-    if object = self.filename_to_ostruct(File.join(rails_root, normal_file))
-      message << "loaded '#{normal_file}'"
-    elsif object = self.filename_to_ostruct(File.join(rails_root, sample_file))
-      message << "loaded '#{sample_file}'"
-    else
-      raise Errno::ENOENT, "Couldn't find '#{normal_file}'"
+    message << "Loading settings from file #{normal_file}. "
+
+    settings = self.filename_to_hash(File.join(rails_root, normal_file))
+    defaults = self.filename_to_hash(File.join(rails_root, sample_file))
+
+    unspecified_keys = []
+
+    defaults.each do |key,value|
+      unless settings.key?(key)
+        unspecified_keys << key
+        settings[key] = value
+      end
+    end
+
+    if unspecified_keys.present?
+      message << "Using default values for unspecified settings " << unspecified_keys.sort.map(&:inspect).to_sentence
     end
 
     RAILS_DEFAULT_LOGGER.info(message) rescue nil
 
-    return object
+    return OpenStruct.new(settings)
   end
 
   # Return an OpenStruct object by reading the +filename+ and parsing it with ERB and YAML.
-  def self.filename_to_ostruct(filename)
-    return OpenStruct.new(YAML.load(ERB.new(File.read(filename)).result)) rescue nil
+  def self.filename_to_hash(filename)
+    return YAML.load(ERB.new(File.read(filename)).result) rescue {}
   end
 end
--- spec/lib/settings_reader_spec.rb
+++ spec/lib/settings_reader_spec.rb
@@ -7,20 +7,36 @@ foo: bar
 FILE
 
     @sample_file = <<FILE
+foo: bob
 bat: baz
+FILE
+
+    @settings_file_all = <<FILE
+foo: bar
+bat: man
 FILE
   end
 
-  it "should use settings.yml if it exists" do
+  it "should use values from settings.yml if specified, and values from settings.yml.example if not" do
     File.stubs(:read).with {|filename| File.basename(filename) == "settings.yml"}.returns(@settings_file)
     File.stubs(:read).with {|filename| File.basename(filename) == "settings.yml.example"}.returns(@sample_file)
+    RAILS_DEFAULT_LOGGER.expects(:info).with {|msg| msg =~ /Using default values for unspecified settings "bat"/}
 
-    SettingsReader.read.should == OpenStruct.new("foo" => "bar")
+    SettingsReader.read.should == OpenStruct.new("foo" => "bar", "bat" => "baz")
   end
 
-  it "should use settings.yml.example if settings.yml does not exist" do
+  it "should use values from settings.yml.example if settings.yml does not exist" do
     File.stubs(:read).with {|filename| File.basename(filename) == "settings.yml.example"}.returns(@sample_file)
+    RAILS_DEFAULT_LOGGER.expects(:info).with {|msg| msg =~ /Using default values for unspecified settings "bat" and "foo"/}
 
-    SettingsReader.read.should == OpenStruct.new("bat" => "baz")
+    SettingsReader.read.should == OpenStruct.new("foo" => "bob", "bat" => "baz")
+  end
+
+  it "should not output a warning if settings.yml defines all settings" do
+    File.stubs(:read).with {|filename| File.basename(filename) == "settings.yml"}.returns(@settings_file_all)
+    File.stubs(:read).with {|filename| File.basename(filename) == "settings.yml.example"}.returns(@sample_file)
+    RAILS_DEFAULT_LOGGER.expects(:info).with {|msg| msg !~ /Using default values/}
+
+    SettingsReader.read.should == OpenStruct.new("foo" => "bar", "bat" => "man")
   end
 end
