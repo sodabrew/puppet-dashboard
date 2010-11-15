c0080fe40306ce4fe8faea03f37ac27c270c43b3
Paul Berry <paul@puppetlabs.com>
2010-11-15 13:52:45 -0800

    (#4605) Renamed date_format setting to datetime_format and added separate date_format.
    Graphs are formatted using date_format.  "Latest report" columns in
    tables are formatted using datetime_format.
    
    Paired-with: Nick Lewis <nick@puppetlabs.com>

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,8 +1,8 @@
 # TODO make format customizable through an external configuration file.
 Time::DATE_FORMATS.update(
   # :default => lambda{|time| time.strftime('%m/%d/%y ') + time.strftime('%I:%M%p').downcase }
-  :default => lambda{|time| time.in_time_zone.strftime(SETTINGS.date_format || '%Y-%m-%d %H:%M %Z') },
-  :date => lambda{|time| time.in_time_zone.strftime('%Y-%m-%d') },
+  :default => lambda{|time| time.in_time_zone.strftime(SETTINGS.datetime_format) },
+  :date => lambda{|time| time.in_time_zone.strftime(SETTINGS.date_format) },
   :time => lambda{|time| time.in_time_zone.strftime('%I:%M%p') }
 )
 
