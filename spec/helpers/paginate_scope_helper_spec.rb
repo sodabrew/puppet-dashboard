b18257435eb1f33a5b0b0e064537e9b148d704e0
Jesse Wolfe <jes5199@gmail.com>
2010-11-19 15:16:16 -0800

    Feature #5142 Per-Page parameter
    Added a per-page parameter to the pagination system
    and created a UI widget to select different page sizes, including an
    'all' option.

--- spec/helpers/paginate_scope_helper_spec.rb
+++ spec/helpers/paginate_scope_helper_spec.rb
@@ -5,5 +5,15 @@ describe PaginateScopeHelper do
     it "should paginate the scope" do
       helper.paginate_scope([1,2,3]).should be_a_kind_of(WillPaginate::Collection)
     end
+
+    it "should pass in the per_page parameter" do
+      params[:per_page] = 2
+      helper.paginate_scope([1,2,3]).per_page.should == 2
+    end
+
+    it "should not paginate if the per_page parameter is 'all'" do
+      params[:per_page] = 'all'
+      helper.paginate_scope([1,2,3]).should_not be_a_kind_of(WillPaginate::Collection)
+    end
   end
 end
