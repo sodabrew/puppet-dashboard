4a959fd2bfd93dcb5696509325dda6a7437c4f61
Nick Lewis <nick@puppetlabs.com>
2010-12-16 17:28:44 -0800

    (#5493) Added Puppet::Transaction::Report#to_hash method
    Also added helper methods for the child objects, and to calculate
    some of the necessary fields. This currently does some of the work
    of bringing reports up to the same format, but not all of it.
    
    Paired-With: Jesse Wolfe

--- lib/puppet/report.rb
+++ lib/puppet/report.rb
@@ -19,12 +19,57 @@ module Puppet #:nodoc:
         end
         result
       end
+
+      def to_hash
+        hash = {
+          "report_format" => report_format,
+          "host" => host,
+          "time" => time,
+          "kind" => kind,
+          "puppet_version" => puppet_version,
+          "configuration_version" => configuration_version,
+          "logs" => logs.map(&:to_hash),
+          "metrics" => metrics.values.map(&:to_hash).inject({},&:merge)
+        }
+      end
+
+      def configuration_version_from_log_objects
+        logs.each do |log|
+          if log.version and log.source != "Puppet"
+            return log.version.to_s
+          end
+        end
+        nil
+      end
+
+      def configuration_version_from_log_message
+        logs.each do |log|
+          if log.message =~ /^Applying configuration version '(.*)'$/
+            return $1
+          end
+        end
+        nil
+      end
     end
 
     class Event
       attr_reader :name, :default_log_level, :property, :line, :resource,
         :desired_value, :time, :tags, :version, :source_description, :file,
         :status, :previous_value, :message
+
+      def to_hash
+        {
+          "previous_value"     => previous_value,
+          "desired_value"      => desired_value,
+          "message"            => message,
+          "name"               => name.to_s,
+          "property"           => property,
+          "source_description" => source_description,
+          "status"             => status,
+          "tags"               => tags,
+          "time"               => time
+        }
+      end
     end
   end
 
@@ -37,10 +82,28 @@ module Puppet #:nodoc:
         value = @values.find { |v| v[0] == name }
         value && value[2]
       end
+
+      def to_hash
+        {
+          name.to_s => values.map {|key,_,value| {key.to_s => value} }.inject({},&:merge)
+        }
+      end
     end
 
     class Log
       attr_reader :file, :level, :line, :message, :source, :tags, :time, :version
+
+      def to_hash
+        {
+          "file" => file,
+          "level" => level.to_s,
+          "line" => line,
+          "message" => message,
+          "source" => source,
+          "tags" => tags,
+          "time" => time,
+        }
+      end
     end
   end
 
@@ -73,9 +136,20 @@ module ReportExtensions #:nodoc:
         []
       end
 
-      # 0.25 reports don't have kind but this makes the interface more consistent
       def kind
-        nil
+        "apply"
+      end
+
+      def report_format
+        0
+      end
+
+      def puppet_version
+        "0.25.x"
+      end
+
+      def configuration_version
+        configuration_version_from_log_objects || configuration_version_from_log_message
       end
     end
 
@@ -98,6 +172,40 @@ module ReportExtensions #:nodoc:
 
       # Attributes in 2.6.x but not 0.25.x
       attr_reader :external_times, :resource_statuses, :kind
+
+      def to_hash
+        hash = super
+        hash["resource_statuses"] = resource_statuses.values.map(&:to_hash)
+        hash
+      end
+
+      def kind
+        "apply"
+      end
+
+      def report_format
+        1
+      end
+
+      def puppet_version
+        logs.each do |log|
+          if log.version and log.source == "Puppet"
+            return log.version
+          end
+        end
+        "2.6.x"
+      end
+
+      def configuration_version
+        configuration_version_from_resource_statuses || configuration_version_from_log_objects || configuration_version_from_log_message 
+      end
+
+      def configuration_version_from_resource_statuses
+        resource_statuses.values.each do |resource_status|
+          return resource_status.version.to_s if resource_status.version
+        end
+        nil
+      end
     end
 
     module Resource
@@ -105,6 +213,24 @@ module ReportExtensions #:nodoc:
         attr_reader :source_description, :evaluation_time, :resource, :tags,
           :file, :events, :time, :line, :version, :changed, :change_count,
           :out_of_sync
+
+        def to_hash
+          resource =~ /^(.+?)\[(.+)\]$/
+          resource_type, title = $1, $2
+          {
+            "resource_type" => resource_type,
+            "title" => title,
+            "evaluation_time" => evaluation_time,
+            "file" => file,
+            "line" => line,
+            "source_description" => source_description,
+            "tags" => tags,
+            "time" => time,
+            "change_count" => change_count || 0,
+            "out_of_sync" => out_of_sync,
+            "events" => events.map(&:to_hash)
+          }
+        end
       end
     end
 
