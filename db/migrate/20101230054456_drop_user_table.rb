ed3285a3708447e12a11d7a19b8ab15118751d2e
Matt Robinson <matt@puppetlabs.com>
2010-12-29 21:59:28 -0800

    (#5725) Remove user related code
    There’s a lot of code in Dashboard related to users, and none of it is being
    used. It looks like user code was added despite never really working or being
    required. Users are surely a goal in some form for dashboard – someday. But for
    now it’s just cruft. Also, if we want to upgrade to Rails 3 before we add
    users, this crufty code makes it harder.

---
+++ db/migrate/20101230054456_drop_user_table.rb
@@ -0,0 +1,27 @@
+class DropUserTable < ActiveRecord::Migration
+  def self.up
+    drop_table :users
+  end
+
+  def self.down
+    create_table :users do |t|
+      t.string    :login,               :null => false                # optional, you can use email instead, or both
+      t.string    :email
+      t.string    :crypted_password,    :null => false                # optional, see below
+      t.string    :password_salt,       :null => false                # optional, but highly recommended
+      t.string    :persistence_token,   :null => false                # required
+      t.string    :single_access_token, :null => false                # optional, see Authlogic::Session::Params
+      t.string    :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
+
+      t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
+      t.integer   :failed_login_count,  :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
+      t.datetime  :last_request_at                                    # optional, see Authlogic::Session::MagicColumns
+      t.datetime  :current_login_at                                   # optional, see Authlogic::Session::MagicColumns
+      t.datetime  :last_login_at                                      # optional, see Authlogic::Session::MagicColumns
+      t.string    :current_login_ip                                   # optional, see Authlogic::Session::MagicColumns
+      t.string    :last_login_ip                                      # optional, see Authlogic::Session::MagicColumns
+
+      t.timestamps
+    end
+  end
+end
