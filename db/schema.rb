627612cba06aee3d727a54368638e26b1a766de2
Matt Robinson <matt@puppetlabs.com>
2011-01-03 16:09:19 -0800

    (#5725) Update schema.rb to reflect the removal of the user table
    Reviewed-by: Paul Berry <paul@puppetlabs.com>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101229023023) do
+ActiveRecord::Schema.define(:version => 20101230054456) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "node_id"
@@ -172,25 +172,6 @@ ActiveRecord::Schema.define(:version => 20101229023023) do
     t.integer  "subject_id"
     t.integer  "actor_id"
     t.integer  "secondary_subject_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "login",                              :null => false
-    t.string   "email"
-    t.string   "crypted_password",                   :null => false
-    t.string   "password_salt",                      :null => false
-    t.string   "persistence_token",                  :null => false
-    t.string   "single_access_token",                :null => false
-    t.string   "perishable_token",                   :null => false
-    t.integer  "login_count",         :default => 0, :null => false
-    t.integer  "failed_login_count",  :default => 0, :null => false
-    t.datetime "last_request_at"
-    t.datetime "current_login_at"
-    t.datetime "last_login_at"
-    t.string   "current_login_ip"
-    t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
