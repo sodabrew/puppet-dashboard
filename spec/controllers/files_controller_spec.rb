cca236b58ec8b50dc36061061bd73394f8b930cd
Paul Berry <paul@puppetlabs.com>
2011-01-14 14:35:16 -0800

    (#5171) Allow Dashboard to contact a file bucket server for file diffs
    This feature is currently not exposed through the UI.  We expect to
    add AJAX logic to allow the user to view file diffs.
    
    Paired-with: Nick Lewis <nick@puppetlabs.com>

---
+++ spec/controllers/files_controller_spec.rb
@@ -0,0 +1,40 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe FilesController do
+  describe "#diff" do
+    before :each do
+      SETTINGS.stubs(:use_file_bucket_diffs).returns(true)
+      SETTINGS.stubs(:file_bucket_server).returns('filebucket')
+      SETTINGS.stubs(:file_bucket_port).returns(1337)
+      @options = {:file1 => '24d27c169c2c881eb09a065116f2aa5c', :file2 => '40bb25658a72f731a6f71ef9476cd5af'}
+    end
+
+    it "Forwards the request to the file bucket server" do
+      PuppetHttps.expects(:get).with("https://filebucket:1337/production/file_bucket_file/md5/24d27c169c2c881eb09a065116f2aa5c?diff_with=40bb25658a72f731a6f71ef9476cd5af", 's').returns("This is the diff")
+
+      get :diff, @options
+
+      response.should be_success
+      response.body.should == "This is the diff"
+    end
+
+    it "Doesn't attempt to do a diff if use_file_bucket_diffs is turned off" do
+      SETTINGS.stubs(:use_file_bucket_diffs).returns(false)
+      PuppetHttps.expects(:get).never
+
+      get :diff, @options
+
+      response.should_not be_success
+    end
+
+    [:file1, :file2].each do |which_parameter|
+      it "Rejects an invalid md5 for #{which_parameter}" do
+        @options[which_parameter] = 'Turkmenistan'
+
+        get :diff, @options
+
+        response.should_not be_success
+      end
+    end
+  end
+end
