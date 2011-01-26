f1bed8fa5b5ac26eb35d3e78e15945eb8c1704b6
Jesse Wolfe <jes5199@gmail.com>
2011-01-26 14:34:20 -0800

    maint: Privatize string helper
    This method was showing up as a public controller method, which was
    unintentional.
    
    Reviewed-by: Paul Berry <paul@puppetlabs.com>

--- app/helpers/string_helper.rb
+++ app/helpers/string_helper.rb
@@ -1,4 +1,5 @@
 module StringHelper
+  private
   def is_md5?(str)
     !!(str =~ /^[0-9a-f]{32}$/)
   end
--- spec/helpers/string_helper_spec.rb
+++ spec/helpers/string_helper_spec.rb
@@ -3,23 +3,23 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe StringHelper do
   describe ".is_md5?" do
     it "returns true for a valid md5" do
-      helper.is_md5?("abcdef0123456789abcdef0123456789").should == true
+      helper.instance_eval{ is_md5?("abcdef0123456789abcdef0123456789") }.should == true
     end
 
     it "returns false for a value that is too short" do
-      helper.is_md5?("abcdef0123456789").should == false
+      helper.instance_eval{ is_md5?("abcdef0123456789") }.should == false
     end
 
     it "returns false for a value that is too long" do
-      helper.is_md5?("abcdef012345678abcdef01234567899abcdef0123456789").should == false
+      helper.instance_eval{ is_md5?("abcdef012345678abcdef01234567899abcdef0123456789") }.should == false
     end
 
     it "returns false for a value that contains illegal characters" do
-      helper.is_md5?("this is not an md5").should == false
+      helper.instance_eval{ is_md5?("this is not an md5") }.should == false
     end
 
     it "returns false for upper-case values" do
-      helper.is_md5?("ABCDEF0123456789ABCDEF0123456789").should == false
+      helper.instance_eval{ is_md5?("ABCDEF0123456789ABCDEF0123456789") }.should == false
     end
   end
 end
