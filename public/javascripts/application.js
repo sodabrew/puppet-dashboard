80c182cd50ba2fbe2d0447d50e5a6d82fcfbc6d3
Jesse Wolfe <jes5199@gmail.com>
2011-01-26 15:04:14 -0800

    Maint: Add JQuery UI animation to expand/collapse widgets.
    Reviewed-by: Paul Berry <paul@puppetlabs.com>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -124,7 +124,7 @@ function toggle_expandable_link() {
   jQuery(this).toggleClass('collapsed-link');
   if (jQuery(this).hasClass('collapsed-link')) {
     jQuery(this.id.replace('expand', '#expandable'))
-      .addClass('collapsed');
+      .hide('blind');
     if (jQuery('.expandable-link').not('.collapsed-link').size() == 0) {
       var old_text = jQuery('.collapse-all').text();
       jQuery('.collapse-all')
@@ -134,7 +134,7 @@ function toggle_expandable_link() {
     }
   } else {
     jQuery(this.id.replace('expand', '#expandable'))
-      .removeClass('collapsed');
+      .show('blind', {}, 1000);
     if (jQuery('.expandable-link.collapsed-link').size() == 0) {
       var old_text = jQuery('.expand-all').text();
       jQuery('.expand-all')
