2112ba1721e02ae7041d2e8ba48c3ef348f2a6c0
Paul Berry <paul@puppetlabs.com>
2011-01-27 12:16:18 -0800

    (#5865) Further improvements and bug fixes to the "search inspect reports" page
    Fixed a bug that caused the page to always display the "please specify
    the file title" error.  Removed the radio buttons for
    unmatching/matching/all and instead displayed two results tables:
    matching nodes and unmatching nodes.  Made sure that a table header
    always appears even if there are no matching/unmatching nodes.
    
    Paired-with: Jesse Wolfe <jesse@puppetlabs.com>

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -52,31 +52,27 @@ class ReportsController < InheritedResources::Base
 
   def search
     flash[:errors] = []
-    flash[:notices] = []
     inspected_resources = ResourceStatus.latest_inspections.order("nodes.name")
-
-    @files = []
 
     @title = params[:file_title].to_s.strip
     @content = params[:file_content].to_s.strip
 
-    if @title.present? then
-      @files = case params[:search_results]
-      when 'unmatching' then
-        inspected_resources.by_file_title(@title).without_file_content(@content)
-      when 'matching' then
-        inspected_resources.by_file_title(@title).by_file_content(@content)
-      when 'all' then
-        inspected_resources.by_file_title(@title)
-      else
-        []
+    if params[:file_title] == nil and params[:file_content] == nil
+      # Don't do anything; user just navigated to the search page
+    else
+      if !@title.present?
+        flash[:errors] << "Please specify the file title to search for"
+      end
+      if !@content.present?
+        flash[:errors] << "Please specify the file content to search for"
+      elsif !is_md5?(@content)
+        flash[:errors] << "#{@content} is not a valid md5 checksum"
+      end
+      if flash[:errors].empty?
+        @matching_files = inspected_resources.by_file_title(@title).by_file_content(@content)
+        @unmatching_files = inspected_resources.by_file_title(@title).without_file_content(@content)
       end
     end
-
-    flash[:errors] << "Please specify the file title to search for" if params[:search_results].present?
-    flash[:notices] << "#{@content} is not a valid md5 checksum" if @content.present? and !is_md5?(@content)
-
-    @files = paginate_scope @files
   end
 
   private
