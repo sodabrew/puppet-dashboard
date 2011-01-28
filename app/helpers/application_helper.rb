ea2368f60d53d4578706476bd9f2f596358eda62
Paul Berry <paul@puppetlabs.com>
2011-01-28 15:46:30 -0800

    (#6069) Fixed unique ids in the report group diff view.
    When diffing all the nodes in a group against baselines, the "expand"
    links were failing to work properly because every time the _diff
    partial was executed, it re-used id's that were supposed to be unique
    on the page.  Changed the mechanism for generating unique ids to a
    method generate_unique_id() in the application helper, so that the ids
    will always be unique on the page.
    
    Paired-with: Nick Lewis <nick@puppetlabs.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -261,4 +261,9 @@ module ApplicationHelper
   def load_asynchronously(element, url)
     javascript = "jQuery.get('#{url}', function(data) { jQuery('#{element}').html(data) })"
   end
+
+  def generate_unique_id
+    @unique_id_counter ||= 0
+    @unique_id_counter += 1
+  end
 end
