34267637fde36aee36f2ec5398de3d9bcf69db68
Matt Robinson <matt@puppetlabs.com>
2011-01-27 22:53:57 -0800

    maint: Use new factory_girl syntax to improve a test
    Now that we can define multiple, named factories we can declare
    different test data scenarios more succinctly.  I was also able to
    remove a bunch of unncessary test data that wasn't helping the test.
    
    Reviewed-By: Nick Lewis

--- spec/factories.rb
+++ spec/factories.rb
@@ -28,6 +28,14 @@ Factory.define :report do |report|
   report.time   { Factory.next(:time) }
 end
 
+Factory.define :inspect_report, :parent => :report do |inspect|
+  inspect.kind 'inspect'
+end
+
+Factory.define :baseline_inspect_report, :parent => :inspect_report do |baseline|
+  baseline.after_create {|report| report.baseline!}
+end
+
 Factory.define :resource_status do |status|
 end
 
