8d7f875450230a3380624a4bf67446ee1a753545
Max Martin <max@puppetlabs.com>
2011-03-24 12:11:08 -0700

    (#6531) Folded into one file & renamed
    Took the script functionality from populate_dashboard.rb and added it to
    generate_report.rb, then renamed the file to generate_sample_reports.rb.
    This makes it easier to run the script from any directory without
    require & relative paths causing problems.
    
    Paired-with:Matt Robinson

--- ext/report_generator/generate_sample_reports.rb
+++ ext/report_generator/generate_sample_reports.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/env ruby
 
 require 'erb'
-require 'yaml'
 require 'ostruct'
 require 'rubygems'
 require 'active_support/all'
+require 'YAML'
+require 'fileutils'
+begin
+  require 'puppet/util/zaml'
+  YAML = ZAML
+rescue LoadError
+end
 
 module Puppet; end
 module Puppet::Resource; end
@@ -166,3 +172,22 @@ class Array
     result
   end
 end
+
+FileUtils.mkdir_p('yaml')
+rakefile_if_specified = "-f #{ARGV.first}" unless ARGV.empty?
+
+100.times do
+  report = DataGenerator.generate_report
+  File.open("yaml/#{report.host}.yaml","w") do |f|
+    f.print YAML.dump(report)
+  end
+end
+
+#puts "Importing reports"
+#`rake #{rakefile_if_specified} reports:import REPORT_DIR=yaml`
+
+#puts "Creating unresponsive nodes"
+#10.times do
+#  puts `rake #{rakefile_if_specified} node:add name=#{DataGenerator.generate_hostname}`
+#end
+
