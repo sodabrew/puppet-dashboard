8ff11bee8ff79f7f38fb4eff069ee9048fb99de3
Nick Lewis <nick@puppetlabs.com>
2011-04-21 13:54:54 -0700

    (#7161) Add a route to create node group memberships
    This route is JSON-only, and accepts input either of the form:
    
    {"node_group": {"node_id": <id>, "node_group_id": <id>}}
    or
    {"node_name": <name>, "group_name": <name>}
    
    If a membership already exists, the request will be rejected.
    
    Reviewed-By: Pieter van de Bruggen

--- spec/models/node_group_membership_spec.rb
+++ spec/models/node_group_membership_spec.rb
@@ -3,4 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe NodeGroupMembership do
   it { should belong_to(:node) }
   it { should belong_to(:node_group) }
+
+  it "should only allow a single association between the same node and group" do
+    node = Node.generate!
+    node_group = NodeGroup.generate!
+    membership = NodeGroupMembership.create(:node => node, :node_group => node_group)
+
+    membership.should be_valid
+
+    duplicate = NodeGroupMembership.create(:node => node, :node_group => node_group)
+    duplicate.should_not be_valid
+  end
 end
