15bba313d223e7db96eab253449ffe9a8515bf67
Pieter van de Bruggen <pieter@puppetlabs.com>
2011-07-06 13:08:20 -0700

    (#8121) Properly generate CSS from SASS in production.
    According to this link (http://blog.citrusbyte.com/2008/03/20/sass-production-woes-in-rails/),
    Sass has known issues with CSS generation in Rails 2.3.  This is
    remedied easily enough by providing an initializer that forces the
    plugin to "reload" a little later in the process, but the exact
    mechanism isn't completely understood.  This is an interaction
    between the Haml and Sass gems, since Haml vendors Sass.  When we
    upgrade Haml to 3.2.0, we should be able to remove this
    initializer.
    
    This change may negatively impact the first page load on a new
    server, but the change is fairly minimal.
    
    Reviewed-By: Matt Robbinson

---
+++ config/initializers/sass.rb
@@ -0,0 +1,13 @@
+p "** Ensuring that Sass will regenerate"
+
+# Sass fails to regenerate in production mode at an appropriate time, which
+# yields a server error (500), followed by some unstyled content.  Unsetting
+# this constant in an initializer prompts Sass to ensure that the stylesheets
+# are generated at the appropriate time.  This only applies to the initial
+# page load; subsequent requests are appropriately cached and will exhibit the
+# former behavior when provoked.
+if defined?(Sass::RAILS_LOADED)
+  module Sass
+    remove_const(:RAILS_LOADED)
+  end
+end
