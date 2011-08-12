9b4bdeea070bf5b800e3706b2f8f8c2e2fab5bb7
Jacob Helwig <jacob@puppetlabs.com>
2011-08-12 13:38:10 -0700

    (#7934) Only link old file checksum to file bucket when contents have changed
    The file bucket is only populated with the old contents of a file,
    when the file is changed by Puppet.  Since the new contents of the
    file are not put into the file bucket, the links that were shown for
    the new file contents would only work if there was a file previously
    had that particular checksum.
    
    Now, we only make the checksum a link to view the contents from the
    file bucket if the checksum is for the old file contents, and do not
    show the link for the new file contents, or if the file was created in
    that puppet run.
    
    This does mean that we are hiding the link in cases where there is a
    chance that the file contents could actually be shown.  For example,
    if we are looking at an old report where a file was created, but the
    contents were changed in later Puppet runs.

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,8 +1,13 @@
 module ReportsHelper
   def popup_md5s( string, label = nil )
     if SETTINGS.use_file_bucket_diffs
-      string.gsub(/\{md5\}[a-fA-F0-9]{32}/) do |match|
-        link_to_function label || match, "display_file_popup('#{url_for :controller => :files, :action => :show, :file => match.sub('{md5}','')}')", :class => 'popup-md5'
+      string.sub(/content changed '(\{md5\}[a-fA-F0-9]{32})'/) do |match|
+        hash = $1
+        label ||= hash
+        filebucket_url = url_for(:controller => :files, :action => :show, :file => hash.sub('{md5}',''))
+        function_link = link_to_function(label, "display_file_popup('#{filebucket_url}')", :class => 'popup-md5')
+
+        "content changed '#{function_link}'"
       end
     else
       string
