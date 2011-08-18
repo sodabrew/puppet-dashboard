fa8d27c89bc6dbb04c09236d89570f8a6d9d12b9
Matt Robinson <matt@puppetlabs.com>
2011-08-18 14:05:20 -0700

    (#7934) Give a better error message when filebucket contents don't exist
    Before if the filebucket content didn't exist when you clicked the link
    to it, it would raise an exception that we wouldn't catch, so in
    production you'd get a generic error message.  Now it will tell you the
    filebucket content doesn't exist and link to documentation on how to
    enable the file bucket.
    
    Also fixed some other bugs such as rendering text if you passed an
    invalid md5 and improved test robustness.
    
    Based on code from Jacob Helwig <jacob@puppetlabs.com>
    Reviewed-by: Nick Lewis <nick@puppetlabs.com>

--- spec/controllers/files_controller_spec.rb
+++ spec/controllers/files_controller_spec.rb
@@ -13,7 +13,9 @@ describe FilesController do
     end
 
     it "Forwards the request to the file bucket server" do
-      PuppetHttps.expects(:get).with("https://filebucket:1337/production/file_bucket_file/md5/24d27c169c2c881eb09a065116f2aa5c?diff_with=40bb25658a72f731a6f71ef9476cd5af", 's').returns("This is the diff")
+      PuppetHttps.expects(:get).
+        with("https://filebucket:1337/production/file_bucket_file/md5/24d27c169c2c881eb09a065116f2aa5c?diff_with=40bb25658a72f731a6f71ef9476cd5af", 's').
+        returns("This is the diff")
 
       get :diff, @options
 
@@ -28,6 +30,8 @@ describe FilesController do
       get :diff, @options
 
       response.should_not be_success
+      response.status.should == '403 Forbidden'
+      response.body.should == 'File bucket diffs have been disabled'
     end
 
     [:file1, :file2].each do |which_parameter|
@@ -37,6 +41,8 @@ describe FilesController do
         get :diff, @options
 
         response.should_not be_success
+        response.status.should == '400 Bad Request'
+        response.body.should == 'Invalid md5: "Turkmenistan"'
       end
     end
   end
@@ -44,10 +50,13 @@ describe FilesController do
   describe "#show" do
     before :each do
       @options = {:file => '24d27c169c2c881eb09a065116f2aa5c'}
+      @url = "https://filebucket:1337/production/file_bucket_file/md5/24d27c169c2c881eb09a065116f2aa5c"
     end
 
     it "Forwards the request to the file bucket server" do
-      PuppetHttps.expects(:get).with("https://filebucket:1337/production/file_bucket_file/md5/24d27c169c2c881eb09a065116f2aa5c", 's').returns("This is the contents")
+      PuppetHttps.expects(:get).
+        with("https://filebucket:1337/production/file_bucket_file/md5/24d27c169c2c881eb09a065116f2aa5c", 's').
+        returns("This is the contents")
 
       get :show, @options
 
@@ -62,14 +71,84 @@ describe FilesController do
       get :show, @options
 
       response.should_not be_success
+      response.status.should == '403 Forbidden'
+      response.body.should == 'File bucket diffs have been disabled'
     end
 
     it "Rejects an invalid md5 for :file" do
       @options[:file] = 'Turkmenistan'
 
-      get :diff, @options
+      get :show, @options
 
       response.should_not be_success
+      response.status.should == '400 Bad Request'
+      response.body.should == 'Invalid md5: "Turkmenistan"'
+    end
+
+    it 'gracefully handles Net::HTTPServerExceptions related to cert problems' do
+      PuppetHttps.expects(:get).
+        with(@url, 's').
+        raises(
+          Net::HTTPServerException.new('Forbidden request: localhost(127.0.0.1) access to /file_bucket_file/md5/24d27c169c2c881eb09a065116f2aa5c [find] at line 0',
+            Net::HTTPForbidden.new('foo','403','baz')
+          )
+        )
+
+      get :show, @options
+
+      response.should_not be_success
+      response.status.should == '403'
+      response.body.should have_tag(
+        "p",
+        "Connection not authorized: Forbidden request: localhost(127.0.0.1) " +
+        "access to /file_bucket_file/md5/24d27c169c2c881eb09a065116f2aa5c [find] at line 0"
+      )
+    end
+
+    it 'gracefully handles Net::HTTPServerExceptions related to filebucket content' do
+      PuppetHttps.expects(:get).
+        with(@url, 's').
+        raises(
+          Net::HTTPServerException.new('Could not find file_bucket_file md5/24d27c169c2c881eb09a065116f2aa5c',
+            Net::HTTPNotFound.new('foo','404','baz')
+          )
+        )
+
+      get :show, @options
+
+      response.should_not be_success
+      response.status.should == '404'
+      response.body.should have_tag(
+        "p",
+        "File contents not available: Could not find file_bucket_file md5/24d27c169c2c881eb09a065116f2aa5c"
+      )
+    end
+
+    it 'gracefully handles Errno::ECONNREFUSED when the puppet master is not running' do
+      PuppetHttps.expects(:get).
+        with(@url, 's').
+        raises(Errno::ECONNREFUSED, "your server isn't running, so you don't need to catch it, yo.")
+
+      get :show, @options
+
+      response.should_not be_success
+      response.status.should == '500 Internal Server Error'
+      response.body.should have_tag(
+        "p",
+        "Could not connect to your filebucket server at filebucket:1337"
+      )
+    end
+
+    it 'gracefully handles Exceptions, returning a 500 status code' do
+      PuppetHttps.expects(:get).
+        with(@url, 's').
+        raises(ArgumentError, 'oops')
+
+      get :show, @options
+
+      response.should_not be_success
+      response.status.should == '500 Internal Server Error'
+      response.body.should == 'oops'
     end
   end
 end
