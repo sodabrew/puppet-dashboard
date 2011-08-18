7b742e9ff2b24174f78a79f1c432d23f2e113539
Matt Robinson <matt@puppetlabs.com>
2011-08-18 13:37:43 -0700

    (#7934) Don't link md5s for new content
    When running puppet agent when remote filebucketing is enabled, the old
    content is file bucketed when there's a change, but not the new content.
    Therefore, we won't link to the new content.
    
    There's still plenty of potential to be wrong with the link.  The new
    content would be there if an inspect run was done with --archive_files
    after the apply report, and then we wouldn't link even though the
    content was there.
    
    We also will link when remote filebucketing isn't being used by the
    agents (which may be more often than we thought since there's a bug
    preventing it from working with the agent (#5362), or if the inspect
    runs don't have --archive_files set (which defaults to false).
    
    The baseline plugin will update its usage of popup_md5s as part of
    ticket #9105.
    
    Reviewed-by: Nick Lewis <nick@puppetlabs.com>

--- spec/helpers/reports_helper_spec.rb
+++ spec/helpers/reports_helper_spec.rb
@@ -1,11 +1,53 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ReportsHelper do
+  describe "#popup_md5s" do
+    before do
+      SETTINGS.stubs(:use_file_bucket_diffs).returns(true)
+      @text = "content changed '{md5}b84b7c77fb71f0d945f186513a09e185' to '{md5}d28d2d3560fa76f0dbb1a452f8c38169'"
+    end
 
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(ReportsHelper)
+    it "should make anything that looks like an md5 into a filebucket link" do
+      helper.popup_md5s(@text).gsub(/\s/, '').should == <<-HEREDOC.gsub(/\s/, '')
+        content changed '<a
+          class=\"popup-md5\"
+          href=\"#\"
+          onclick=\"display_file_popup( '/files/show?file=b84b7c77fb71f0d945f186513a09e185'); return false;\">
+          {md5}b84b7c77fb71f0d945f186513a09e185
+        </a>' to '<a
+          class=\"popup-md5\"
+          href=\"#\"
+          onclick=\"display_file_popup('/files/show?file=d28d2d3560fa76f0dbb1a452f8c38169'); return false;\">
+          {md5}d28d2d3560fa76f0dbb1a452f8c38169
+        </a>'
+      HEREDOC
+    end
+
+    it "should make the link text be a label if it's passed in" do
+      helper.popup_md5s(@text, :label => 'foo').gsub(/\s/, '').should == <<-HEREDOC.gsub(/\s/, '')
+        content changed '<a
+          class=\"popup-md5\"
+          href=\"#\"
+          onclick=\"display_file_popup( '/files/show?file=b84b7c77fb71f0d945f186513a09e185'); return false;\">
+          foo
+        </a>' to '<a
+          class=\"popup-md5\"
+          href=\"#\"
+          onclick=\"display_file_popup('/files/show?file=d28d2d3560fa76f0dbb1a452f8c38169'); return false;\">
+          foo
+        </a>'
+      HEREDOC
+    end
+
+    it "should not link md5s if they're in the excluded list" do
+      helper.popup_md5s(@text, :exclude_md5s => ['{md5}d28d2d3560fa76f0dbb1a452f8c38169']).gsub(/\s/, '').should == <<-HEREDOC.gsub(/\s/, '')
+        content changed '<a
+          class=\"popup-md5\"
+          href=\"#\"
+          onclick=\"display_file_popup( '/files/show?file=b84b7c77fb71f0d945f186513a09e185'); return false;\">
+          {md5}b84b7c77fb71f0d945f186513a09e185
+        </a>' to '{md5}d28d2d3560fa76f0dbb1a452f8c38169'
+      HEREDOC
+    end
   end
-
 end
