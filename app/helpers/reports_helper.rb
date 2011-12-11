4c22879037801522847c8200214cec45a6057d33
Eric Shamow <eric@puppetlabs.com>
2011-12-11 17:30:35 -0500

    (#7869) Fix missing diff in dashboard Event tab
    Modify event message to replace the word "changed"
    to link to a diff against the previously stored
    version.  Needs some additional logic to prevent
    calling against a diff that wasn't previously
    stored, but this is no different from current
    behavior and should be refactored separately.

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,7 +1,8 @@
 module ReportsHelper
   def popup_md5s( string, options = {} )
     if SETTINGS.use_file_bucket_diffs
-      string.gsub(/\{md5\}[a-fA-F0-9]{32}/) do |match|
+      hashes = string.scan(/\{md5\}[a-fA-F0-9]{32}/)
+      string.gsub(/changed/, link_to_function( "\\&", "display_file_popup('#{url_for :controller => :files, :action => :diff, :file1 => hashes[0].sub('{md5}',''), :file2 => hashes[1].sub('{md5}','')}')", :class => 'popup-md5')).gsub(/\{md5\}[a-fA-F0-9]{32}/) do |match|
         if options[:exclude_md5s] and options[:exclude_md5s].include? match
           match
         else
