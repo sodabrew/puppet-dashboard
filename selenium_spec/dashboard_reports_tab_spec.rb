7d112e1dec521dc9b98cadd3dacae4321e2d6b50
Daniel Sauble <djsauble@puppetlabs.com>
2012-01-12 11:28:22 -0800

    (#10901) Adds selenium tests
    The table of reports should have the correct class applied
    for status icons to get a graphical tipsy tooltip.

---
+++ selenium_spec/dashboard_reports_tab_spec.rb
@@ -0,0 +1,28 @@
+require "./selenium_spec/spec_helper.rb"
+
+describe "the Dashboard Reports tab" do
+
+  before :all do 
+    @driver = get_driver
+    @menu = @driver.find_element(:css => "div#header ul.navigation")
+  end
+
+  after :all do
+    @driver.quit
+  end
+
+  describe 'reports menu item' do
+    it 'should exist' do
+      reports_link = @driver.find_element(:link => "Reports")
+      reports_link.displayed?.should be_true
+      @driver.action.click(reports_link).perform
+    end
+  end
+
+  describe 'reports table' do
+    it 'should allow status icons to be displayed with a graphical tooltip' do
+      @driver.find_element(:css => "table.inspector.main").should be_true
+    end
+  end
+
+end
