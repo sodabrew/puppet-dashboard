856e078e67f04a0c107f13ff680ce78e2841bc4b
Daniel Sauble <djsauble@puppetlabs.com>
2012-04-03 15:42:35 -0700

    Improves font consistency

--- public/javascripts/radiator.js
+++ public/javascripts/radiator.js
@@ -10,15 +10,15 @@ function resizeMe() {
   var displayHeight = $(window).height();
   var percentageHeight = displayHeight / preferredHeight;
 
-  var preferredWidth = 944;
+  var preferredWidth = 1100;
   var displayWidth = $(window).width();
   var percentageWidth = displayWidth / preferredWidth;
 
   var newFontSize;
   if (percentageHeight < percentageWidth) {
-    newFontSize = Math.floor("720" * percentageHeight) - 20;
+    newFontSize = Math.floor("815" * percentageHeight) - 30;
   } else {
-    newFontSize = Math.floor("720" * percentageWidth) - 30;
+    newFontSize = Math.floor("815" * percentageWidth) - 30;
   }
   $("body").css("font-size", newFontSize + "%")
 }
