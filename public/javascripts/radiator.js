fd6f37338659b065f7436747cb3cb266905e3ce9
Daniel Sauble <djsauble@puppetlabs.com>
2012-04-03 15:10:16 -0700

    Fixes aspect ratio for portrait orientations

--- public/javascripts/radiator.js
+++ public/javascripts/radiator.js
@@ -8,7 +8,17 @@ $(function() {
 function resizeMe() {
   var preferredHeight = 944;
   var displayHeight = $(window).height();
-  var percentage = displayHeight / preferredHeight;
-  var newFontSize = Math.floor("800" * percentage) - 20;
+  var percentageHeight = displayHeight / preferredHeight;
+
+  var preferredWidth = 944;
+  var displayWidth = $(window).width();
+  var percentageWidth = displayWidth / preferredWidth;
+
+  var newFontSize;
+  if (percentageHeight < percentageWidth) {
+    newFontSize = Math.floor("720" * percentageHeight) - 20;
+  } else {
+    newFontSize = Math.floor("720" * percentageWidth) - 30;
+  }
   $("body").css("font-size", newFontSize + "%")
 }
