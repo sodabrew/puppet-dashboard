54b971457c42c3debb18a30541f9112f3e59fa89
Ken J <kfj@puppetlabs.com>
2012-09-13 11:35:07 -0700

    Modified to add the additional behavior in a saner way per dpittman's suggestion.

--- config/initializers/mysql_session_sql_mode.rb
+++ config/initializers/mysql_session_sql_mode.rb
@@ -1,15 +1,11 @@
 module ActiveRecord::ConnectionAdapters
   class MysqlAdapter
+    alias :configure_connection_old :configure_connection
     def configure_connection
-      encoding = @config[:encoding]
-      execute("SET NAMES '#{encoding}'") if encoding
-      # By default, MySQL 'where id is null' selects the last inserted id.
-      # # Turn this off. http://dev.rubyonrails.org/ticket/6778
-
-      execute("SET SQL_AUTO_IS_NULL=0")
-
       # Explicitly set the SQL mode for the session in case there are problematic global settings
       execute("SET @@SESSION.sql_mode='TRADITIONAL'")
+
+      configure_connection_old()
     end
   end
 end
