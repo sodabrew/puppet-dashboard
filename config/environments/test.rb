aef076b97e2f9add2a6e4d5f725bddb1412d6255
Aaron Stone <aaron@serendipity.cx>
2012-10-18 02:35:19 -0700

    Upgrade environment.rb and environments/

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,40 +1,25 @@
-# Settings specified here will take precedence over those in config/environment.rb
+PuppetDashboard::Application.configure do
+  # Settings specified here will take precedence over those in config/environment.rb
 
-# The test environment is used exclusively to run your application's
-# test suite.  You never need to work with it otherwise.  Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs.  Don't rely on the data there!
-config.cache_classes = true
+  # The test environment is used exclusively to run your application's
+  # test suite.  You never need to work with it otherwise.  Remember that
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs.  Don't rely on the data there!
+  config.cache_classes = true
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
-config.action_view.cache_template_loading            = true
+  # Show full error reports and disable caching
+  config.action_controller.consider_all_requests_local = true
+  config.action_controller.perform_caching             = false
+  config.action_view.cache_template_loading            = true
 
-# Disable request forgery protection in test environment
-config.action_controller.allow_forgery_protection    = false
+  # Disable request forgery protection in test environment
+  config.action_controller.allow_forgery_protection    = false
 
-# Tell Action Mailer not to deliver emails to the real world.
-# The :test delivery method accumulates sent emails in the
-# ActionMailer::Base.deliveries array.
-config.action_mailer.delivery_method = :test
-
-# Use SQL instead of Active Record's schema dumper when creating the test database.
-# This is necessary if your schema can't be completely dumped by the schema dumper,
-# like if you have constraints or database-specific column types
-# Unfortunately, this doesn't work
-# A fix was merged recently
-# https://github.com/rails/rails/issues/715
-# But not sure when they'll backport it to Rails 2.3
-# https://github.com/rails/rails/issues/743
-# config.active_record.schema_format = :sql
-
-config.gem 'mocha'
-config.gem 'rspec', :lib => false
-config.gem 'rspec-rails', :lib => false
-config.gem 'thoughtbot-shoulda',
-           :lib => false,
-           :source => 'http://gems.github.com'
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+end
