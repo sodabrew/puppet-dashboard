565002db6733830db766bff9eb6f4651daeecad3
Aaron Stone <aaron@serendipity.cx>
2012-10-18 18:11:13 -0700

    Fix ambiguous order by 'name' column.

--- app/models/node_class.rb
+++ app/models/node_class.rb
@@ -15,7 +15,7 @@ class NodeClass < ActiveRecord::Base
   validates_format_of :name, :with => /\A([a-z0-9][-\w]*)(::[a-z0-9][-\w]*)*\Z/, :message => "must contain a valid Puppet class name, e.g. 'foo' or 'foo::bar'"
   validates_uniqueness_of :name
 
-  default_scope :order => 'name ASC'
+  default_scope :order => 'node_classes.name ASC'
 
   scope :search, lambda{|q| q.blank? ? {} : {:conditions => ['name LIKE ?', "%#{q}%"]} }
 
