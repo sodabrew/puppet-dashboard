d87c0e57f72ca004ba4568b27e959d8413228063
Aaron Stone <aaron@serendipity.cx>
2012-10-18 17:26:58 -0700

    Add root/lib to autoloads.

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module PuppetDashboard
     config.filter_parameters += [:password, :report]
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
