18bd7a583437b5cc74c67b68b91575082ab23bfc
Aaron Stone <aaron@serendipity.cx>
2012-10-23 02:58:04 -0700

    Remove unneeded code.

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -2,6 +2,5 @@
 require "#{Rails.root}/lib/has_many_through_dependent"
 require "#{Rails.root}/lib/association_collection_dependent"
 require "#{Rails.root}/lib/has_parameters"
-require "#{Rails.root}/lib/active_model_scopes"
 require "#{Rails.root}/lib/puppet/report"
 require "#{Rails.root}/lib/csv_extensions"
--- lib/active_model_scopes.rb
+++
@@ -1,9 +0,0 @@
-module ActiveModelScopes
-  def self.included(base)
-    base.scope :limit, lambda{|limit| {:limit => limit}}
-    base.scope :where, lambda{|where| {:conditions => where}}
-    base.scope :order, lambda{|order| {:order => order}}
-  end
-end
-
-ActiveRecord::Base.send :include, ActiveModelScopes
