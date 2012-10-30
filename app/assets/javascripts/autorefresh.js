d220279b267b7c1b39e976b69b385e77488ab577
Aaron Stone <aaron@serendipity.cx>
2012-10-29 19:40:48 -0700

    Better autorefresh thing.

---
+++ app/assets/javascripts/autorefresh.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  var refresh_timer;
+
+  $('input[name="autorefresh"]').change(function() {
+    if (this.checked) {
+      var refresh_count = 15; // Refresh after 15 seconds
+      $('span#autorefresh_countdown').text(refresh_count);
+      $.cookie('autorefresh', 'on');
+
+      refresh_timer = setInterval(function() {
+        refresh_count--;
+        if (refresh_count == 1) {
+          $('span#autorefresh_countdown').html('&hellip;');
+        } else if (refresh_count == 0) {
+          window.location.reload();
+        } else {
+          $('span#autorefresh_countdown').text(refresh_count);
+        }
+      }, 1000);
+    } else {
+      clearInterval(refresh_timer);
+      $('span#autorefresh_countdown').text('');
+      $.removeCookie('autorefresh');
+    }
+  });
+
+  if ($.cookie('autorefresh')) {
+    $('input[name="autorefresh"]').prop('checked', true);
+    $('input[name="autorefresh"]').change(); // prop does not fire events
+  }
+});
