fe0bd0bc3ea574c33822825744cf3bdffc8032d9
Aaron Stone <aaron@serendipity.cx>
2012-11-02 12:01:22 -0700

    Fix nodes/index specs for rspec2.

--- spec/views/nodes/index.html.haml_spec.rb
+++ spec/views/nodes/index.html.haml_spec.rb
@@ -3,11 +3,7 @@ require 'spec_helper'
 
 describe '/nodes/index' do
   before :each do
-    assigns[:nodes] = @nodes = [Node.generate!]
-  end
-
-  def do_render
-    render :template => '/nodes/index'
+    @nodes = [Node.generate!]
   end
 
   describe "search fields" do
@@ -19,31 +15,31 @@ describe '/nodes/index' do
     end
 
     it "should not have :current or :successful if these aren't defined" do
-      do_render
-      should_not have_tag('.search input#current')
-      should_not have_tag('.search input#successful')
+      render
+      rendered.should_not have_tag('.search input#current')
+      rendered.should_not have_tag('.search input#successful')
     end
 
     it "should should have only :current if only it's defined" do
       params[:current] = "true"
-      do_render
-      should have_tag('.search input#current[type=hidden][value=true]')
-      should_not have_tag('.search input#successful')
+      render
+      rendered.should have_tag('.search input#current[type=hidden][value=true]')
+      rendered.should_not have_tag('.search input#successful')
     end
 
     it "should should have only :successful if only it's defined" do
       params[:successful] = "false"
-      do_render
-      should_not have_tag('.search input#current')
-      should have_tag('.search input#successful[type=hidden][value=false]')
+      render
+      rendered.should_not have_tag('.search input#current')
+      rendered.should have_tag('.search input#successful[type=hidden][value=false]')
     end
 
     it "should should both :current and :successful if both are defined" do
       params[:current] = "true"
       params[:successful] = "false"
-      do_render
-      should have_tag('.search input#current[type=hidden][value=true]')
-      should have_tag('.search input#successful[type=hidden][value=false]')
+      render
+      rendered.should have_tag('.search input#current[type=hidden][value=true]')
+      rendered.should have_tag('.search input#successful[type=hidden][value=false]')
     end
   end
 end
