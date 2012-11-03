ee2e754d0eea532540ad36ee1b0831819338fbd0
Aaron Stone <aaron@serendipity.cx>
2012-11-03 13:38:00 -0700

    Groups and Classes can be looked up by name in urls.

--- app/controllers/node_classes_controller.rb
+++ app/controllers/node_classes_controller.rb
@@ -4,4 +4,10 @@ class NodeClassesController < InheritedResources::Base
   before_filter :raise_if_enable_read_only_mode, :only => [:new, :edit, :create, :update, :destroy]
 
   include SearchableIndex
+
+  protected
+
+  def resource
+    get_resource_ivar || set_resource_ivar(end_of_association_chain.find_by_id_or_name!(params[:id]))
+  end
 end
