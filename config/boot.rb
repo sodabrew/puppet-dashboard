39703e56aec5af18b844ddf1a5bb499d9707ccb0
Aaron Stone <aaron@serendipity.cx>
2012-12-11 05:28:20 -0800

    Force YAML::ENGINE to 'syck' because psych is horribly broken.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,7 @@
+# Puppet reports do not work with psych. Must set this prior to loading
+# the Gemfile, otherwise delayed_job, etc. would use psych.
+YAML::ENGINE.yamler = 'syck' if RUBY_VERSION >= '1.9'
+
 require 'rubygems'
 
 # Set up gems listed in the Gemfile.
