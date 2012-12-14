d8f305993847e491d2c75d11e8eb7e839899c2cd
Aaron Stone <aaron@serendipity.cx>
2012-12-14 12:14:59 -0800

    Let's see if this help PR #6

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,9 @@
+require 'rubygems'
+
 # Puppet reports do not work with psych. Must set this prior to loading
 # the Gemfile, otherwise delayed_job, etc. would use psych.
+require 'yaml'
 YAML::ENGINE.yamler = 'syck' if RUBY_VERSION >= '1.9'
-
-require 'rubygems'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
