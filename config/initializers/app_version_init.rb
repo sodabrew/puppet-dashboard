5bf5dbb259ffdd41be1ed0a01836bfbf6e150e10
Moses Mendoza <moses@puppetlabs.com>
2013-01-29 10:15:33 -0800

    Remove VERSION file, instead using CHANGELOG for version link
    This commit removes the VERSION file from dashboard, instead relying on the
    CHANGELOG to determine the latest version. This commit retains the behavior of
    the version link, which points to the respective CHANGELOG entry in github, but
    still removes a manual step in the releasing of Puppet Dashboard. With this
    commit, only the CHANGELOG is manually updated, rather than both CHANGELOG and
    VERSION.
    
    Signed-off-by: Moses Mendoza <moses@puppetlabs.com>

--- config/initializers/app_version_init.rb
+++ config/initializers/app_version_init.rb
@@ -1,14 +1,14 @@
 # Get the Dashboard version number.
 #
 # Any officially released package or tarball of dashboard
-# will have a VERSION file that is generated at tarball
+# will have a CHANGELOG file that is generated at tarball
 # build time.
 #
 # This allows for the APP_VERSION to be determined by git, so
 # developers can run out of master after a git checkout.
 def get_app_version
-  if File.exists?(Rails.root.join('VERSION'))
-    return File.read(Rails.root.join('VERSION')).strip
+  if File.exists?(Rails.root.join('CHANGELOG'))
+    return File.open(Rails.root.join('CHANGELOG')) {|f| f.readline}.strip
   elsif File.directory?(Rails.root.join('.git'))
     return `cd '#{Rails.root}'; git describe`.strip! rescue 'unknown'
   end
