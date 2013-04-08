e6bfcef830708031d0380fd552edec13b93f0d7e
fhrbek <filip.hrbek@gmail.com>
2013-04-08 18:25:35 +0200

    Fix the radiator view production mode so that appropriate assets would be precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ PuppetDashboard::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.radiator.js application.radiator.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
