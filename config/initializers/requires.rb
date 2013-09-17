e867bf6a0a1ecc2c40e3b521b969f8bb9bd55677
Aaron Stone <aaron@serendipity.cx>
2013-09-17 11:22:47 -0700

    Rails 3 provides has_many :through, remove this workaround

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -1,5 +1,4 @@
 # Local libraries
-require "#{Rails.root}/lib/has_many_through_dependent"
 require "#{Rails.root}/lib/association_collection_dependent"
 require "#{Rails.root}/lib/has_parameters"
 require "#{Rails.root}/lib/puppet/report"
