eb75d1352a114d3e18995d069dd30c7469e16522
Aaron Stone <aaron@serendipity.cx>
2013-09-18 12:15:59 -0700

    Rails 3.2 has replaced AssociationCollection with CollectionAssociation, so remove this monkey patch

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -1,5 +1,4 @@
 # Local libraries
-require "#{Rails.root}/lib/association_collection_dependent"
 require "#{Rails.root}/lib/has_parameters"
 require "#{Rails.root}/lib/puppet/report"
 require "#{Rails.root}/lib/csv_extensions"
