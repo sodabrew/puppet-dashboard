dfdc37308036bef45b3d0b41a649588fa3230b4b
Aaron Stone <aaron@serendipity.cx>
2014-03-20 22:59:01 -0700

    Revert "Include name in the group by to allow the field to be sorted"
    This reverts commit c7448ab4cfd38c790e5fd0652e225d217e754edd.

--- app/models/node_class.rb
+++ app/models/node_class.rb
@@ -27,7 +27,7 @@ class NodeClass < ActiveRecord::Base
       LEFT OUTER JOIN node_class_memberships ON (node_classes.id = node_class_memberships.node_class_id)
       LEFT OUTER JOIN nodes ON (nodes.id = node_class_memberships.node_id)
     SQL
-    :group => 'node_classes.id, node_classes.name'
+    :group => 'node_classes.id'
 
   def to_param
     SETTINGS.numeric_url_slugs ? id.to_s : name
