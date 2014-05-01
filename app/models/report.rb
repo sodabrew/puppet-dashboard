f86620f0149540ea82ffc8a3d569129973e17dfc
cat-code <wxu@verisign.com>
2014-05-01 17:02:23 -0400

    bug, log message size
    message could grow larger than what database column size allow.

--- app/models/report.rb
+++ app/models/report.rb
@@ -77,7 +77,14 @@ class Report < ActiveRecord::Base
 
   def self.attribute_hash_from(report_hash)
     attribute_hash = report_hash.dup
+    
+    # message could grow larger than what database column size allow.
     attribute_hash["logs_attributes"] = attribute_hash.delete("logs")
+    attribute_hash["logs_attributes"].each do |resource_logs_hash|
+      log_message = resource_logs_hash["message"].to_s
+      resource_logs_hash["message"] = log_message.slice!(0, 65535) if log_message.length > 65535
+    end
+    
     attribute_hash["resource_statuses_attributes"] = attribute_hash.delete("resource_statuses")
     attribute_hash["metrics_attributes"] = attribute_hash.delete("metrics")
     attribute_hash["resource_statuses_attributes"].each do |resource_status_hash|
