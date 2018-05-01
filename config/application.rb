130f0bfddf59078e30a7bb9a7780852ba1f36be6
Andreas Zuber <zuber@puzzle.ch>
2018-05-01 15:38:36 +0200

    Update rails configuration files

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,9 @@ Bundler.require(*Rails.groups)
 
 module PuppetDashboard
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
