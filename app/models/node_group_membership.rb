92bd12ba37b7f5603aed8d4baac1b2fe00e60df3
Mike White <michaeljwhite@gmail.com>
2020-07-13 11:55:56 -0700

    forces NodeGroup creation before NodeGroupMemberships by moving the validations to belongs_to association

--- app/models/node_group_membership.rb
+++ app/models/node_group_membership.rb
@@ -1,9 +1,8 @@
 class NodeGroupMembership < ApplicationRecord
-  validates_presence_of :node_id, :node_group_id
   validates_uniqueness_of :node_id, :scope => :node_group_id
 
-  belongs_to :node
-  belongs_to :node_group
+  belongs_to :node, required: true
+  belongs_to :node_group, required: true
 
   def to_json(*args)
     {"node_group_id" => node_group_id, "node_id" => node_id}.to_json(*args)
